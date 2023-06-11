5cb04e57aaed4ae5a1bc6b51bc42c20967c0744a
Nimish <nimish@phase.dev>
2023-06-11 14:19:22 +0530

    fix: docker file and script env vars

--- frontend/scripts/replace-variable.sh
+++ frontend/scripts/replace-variable.sh
@@ -2,12 +2,6 @@
 
 find /app/public /app/.next -type f -name "*.js" |
 while read file; do
-    sed -i "s|BAKED_NEXTAUTH_SECRET|$NEXTAUTH_SECRET|g" "$file"
+    sed -i "s|BAKED_NEXT_PUBLIC_BACKEND_API_BASE|$NEXT_PUBLIC_BACKEND_API_BASE|g" "$file"
     sed -i "s|BAKED_NEXT_PUBLIC_NEXTAUTH_PROVIDERS|$NEXT_PUBLIC_NEXTAUTH_PROVIDERS|g" "$file"
-    sed -i "s|BAKED_GITHUB_CLIENT_ID|$GITHUB_CLIENT_ID|g" "$file"
-    sed -i "s|BAKED_GITHUB_CLIENT_SECRET|$GITHUB_CLIENT_SECRET|g" "$file"
-    sed -i "s|BAKED_GOOGLE_CLIENT_ID|$GOOGLE_CLIENT_ID|g" "$file"
-    sed -i "s|BAKED_GOOGLE_CLIENT_SECRET|$GOOGLE_CLIENT_SECRET|g" "$file"
-    sed -i "s|BAKED_GITLAB_CLIENT_ID|$GITLAB_CLIENT_ID|g" "$file"
-    sed -i "s|BAKED_GITLAB_CLIENT_SECRET|$GITLAB_CLIENT_SECRET|g" "$file"
 done
