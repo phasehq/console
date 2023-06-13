204d539ef1e6cb174d7211a4a9656af7566e2461
rohan-chaturvedi <rohan.chaturvedi@protonmail.com>
2023-06-13 12:52:18 +0530

    test: remove try / except block

--- backend/api/views.py
+++ backend/api/views.py
@@ -192,11 +192,11 @@ def kms(request, app_id):
         return HttpResponse(status=404)
     try:
         app = App.objects.get(app_token=app_token)
-        try:
-            timestamp = datetime.now().timestamp() * 1000
-            KMSDBLog.objects.create(app_id=app_id, event_type=event_type, phase_node=phase_node, ph_size=ph_size, ip_address=ip_address, timestamp=timestamp)
-        except Exception as ex:
-            print(ex)
+        #try:
+        timestamp = datetime.now().timestamp() * 1000
+        KMSDBLog.objects.create(app_id=app_id, event_type=event_type, phase_node=phase_node, ph_size=ph_size, ip_address=ip_address, timestamp=timestamp)
+        # except Exception as ex:
+        #     print(ex)
             #pass
         return JsonResponse({
             'wrappedKeyShare': app.wrapped_key_share
