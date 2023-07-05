ef04c7ac20cfc247706b20fa9690ee4907c45b58
Nimish <nimish@phase.dev>
2023-07-05 16:22:16 +0530

    changed find and replace script

--- frontend/scripts/replace-variable.sh
+++ frontend/scripts/replace-variable.sh
@@ -1,7 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-find /app/public /app/.next -type f -name "*.js" |
-while read file; do
-    sed -i "s|BAKED_NEXT_PUBLIC_BACKEND_API_BASE|$NEXT_PUBLIC_BACKEND_API_BASE|g" "$file"
-    sed -i "s|BAKED_NEXT_PUBLIC_NEXTAUTH_PROVIDERS|$NEXT_PUBLIC_NEXTAUTH_PROVIDERS|g" "$file"
-done
+# Ensure NEXT_PUBLIC_BACKEND_API_BASE and NEXT_PUBLIC_NEXTAUTH_PROVIDERS are set
+if [ -z "$NEXT_PUBLIC_BACKEND_API_BASE" ]; then
+    echo "NEXT_PUBLIC_BACKEND_API_BASE is not set. Please set it and rerun the script."
+    exit 1
+fi
+
+if [ -z "$NEXT_PUBLIC_NEXTAUTH_PROVIDERS" ]; then
+    echo "NEXT_PUBLIC_NEXTAUTH_PROVIDERS is not set. Please set it and rerun the script."
+    exit 1
+fi
+
+# Go to the directory where Next.js generates its static files
+cd /app/.next
+
+# Find .js files and replace BAKED_NEXT_PUBLIC_BACKEND_API_BASE with the value of NEXT_PUBLIC_BACKEND_API_BASE
+# and BAKED_NEXT_PUBLIC_NEXTAUTH_PROVIDERS with the value of NEXT_PUBLIC_NEXTAUTH_PROVIDERS
+find . -type f -name "*.js" -exec sed -i "s|BAKED_NEXT_PUBLIC_BACKEND_API_BASE|$NEXT_PUBLIC_BACKEND_API_BASE|g" {} \;
+find . -type f -name "*.js" -exec sed -i "s|BAKED_NEXT_PUBLIC_NEXTAUTH_PROVIDERS|$NEXT_PUBLIC_NEXTAUTH_PROVIDERS|g" {} \;
+
+echo "Replacement complete."
