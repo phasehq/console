50c0e353884b51effba270f61d9df256f9eeec8b
rohan-chaturvedi <rohan.chaturvedi@protonmail.com>
2023-07-13 14:34:47 +0530

    fix: use APP_HOST to rewrite client side env var

--- frontend/scripts/replace-variable.sh
+++ frontend/scripts/replace-variable.sh
@@ -15,5 +15,5 @@ find /app/public /app/.next -type f -name "*.js" |
 while read file; do
     sed -i "s|BAKED_NEXT_PUBLIC_BACKEND_API_BASE|$NEXT_PUBLIC_BACKEND_API_BASE|g" "$file"
     sed -i "s|BAKED_NEXT_PUBLIC_NEXTAUTH_PROVIDERS|$NEXT_PUBLIC_NEXTAUTH_PROVIDERS|g" "$file"
-    sed -i "s|BAKED_NEXT_PUBLIC_APP_HOST|$NEXT_PUBLIC_APP_HOST|g" "$file"
+    sed -i "s|BAKED_NEXT_PUBLIC_APP_HOST|$APP_HOST|g" "$file"
 done
