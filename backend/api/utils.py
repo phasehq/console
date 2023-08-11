1fd6550444d9ff3fb66f226a7d20cd8370e688b8
rohan-chaturvedi <rohan.chaturvedi@protonmail.com>
2023-08-11 15:44:48 +0530

    feat: add util to infer env from token

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,3 +1,5 @@
+from api.models import EnvironmentSecret
+
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
@@ -5,3 +7,17 @@ def get_client_ip(request):
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
+
+def get_env_from_token(auth_token):
+    env_token = auth_token.split("Bearer ")[1]
+
+    if not env_token:
+        return False
+    
+    try:
+      env_secret = EnvironmentSecret.objects.get(token=env_token)
+      return env_secret.environment, env_secret.user
+    except Exception as ex:
+        return False
+
+    
