b72fe95e6b0c7b7be0c5b22c9aee75bd1d15c9bc
rohan-chaturvedi <rohan.chaturvedi@protonmail.com>
2023-08-31 13:19:59 +0530

    feat: misc ui improvements to keyring unlock dialog

--- frontend/components/auth/UnlockKeyringDialog.tsx
+++ frontend/components/auth/UnlockKeyringDialog.tsx
@@ -1,11 +1,13 @@
 import { Dialog, Transition } from '@headlessui/react'
 import { Fragment, useContext, useEffect, useState } from 'react'
-import { FaEye, FaEyeSlash, FaTimes } from 'react-icons/fa'
+import { FaEye, FaEyeSlash, FaLock, FaTimes } from 'react-icons/fa'
 import { Button } from '../common/Button'
 import { KeyringContext } from '@/contexts/keyringContext'
 import { cryptoUtils } from '@/utils/auth'
 import { getLocalKeyring } from '@/utils/localStorage'
 import { useSession } from 'next-auth/react'
+import clsx from 'clsx'
+import { toast } from 'react-toastify'
 
 export default function UnlockKeyringDialog(props: { organisationId: string }) {
   const [password, setPassword] = useState<string>('')
@@ -29,12 +31,21 @@ export default function UnlockKeyringDialog(props: { organisationId: string }) {
   const decryptLocalKeyring = async (event: { preventDefault: () => void }) => {
     event.preventDefault()
     const encryptedKeyring = getLocalKeyring(props.organisationId)
-    if (!encryptedKeyring) throw 'Error fetching local encrypted keys from browser'
-    const deviceKey = await cryptoUtils.deviceVaultKey(password, session?.user?.email!)
-    const decryptedKeyring = await cryptoUtils.decryptAccountKeyring(encryptedKeyring!, deviceKey)
-    if (!decryptedKeyring) throw 'Failed to decrypt keys'
-    setKeyring(decryptedKeyring)
-    closeModal()
+    if (!encryptedKeyring) {
+      toast.error('Error fetching local encrypted keys from browser')
+      return false
+    }
+    try {
+      const deviceKey = await cryptoUtils.deviceVaultKey(password, session?.user?.email!)
+      const decryptedKeyring = await cryptoUtils.decryptAccountKeyring(encryptedKeyring!, deviceKey)
+      setKeyring(decryptedKeyring)
+      toast.success('Unlocked user keyring!', { autoClose: 2000 })
+      closeModal()
+    } catch (e) {
+      console.log(`Error unlocking user keyring: ${e}`)
+      toast.error('Failed to decrypt keys. Please verify your sudo password and try again.')
+      return false
+    }
   }
 
   return (
@@ -65,38 +76,52 @@ export default function UnlockKeyringDialog(props: { organisationId: string }) {
                 leaveTo="opacity-0 scale-95"
               >
                 <Dialog.Panel className="w-full max-w-2xl transform overflow-hidden rounded-2xl bg-neutral-100 dark:bg-neutral-900 p-6 text-left align-middle shadow-xl transition-all">
-                  <Dialog.Title as="div" className="flex w-full justify-between">
+                  <Dialog.Title as="div" className="flex w-full gap-2 items-center">
+                    <FaLock
+                      className={clsx(
+                        keyring === null ? 'text-red-500' : 'text-emerald-500',
+                        'transition-colors ease'
+                      )}
+                    />
                     <h3 className="text-lg font-medium leading-6 text-black dark:text-white ">
                       Unlock User Keyring
                     </h3>
                   </Dialog.Title>
                   <form onSubmit={decryptLocalKeyring}>
-                    <div className="space-y-4 max-w-md mx-auto">
-                      <label
-                        className="block text-gray-700 text-sm font-bold mb-2"
-                        htmlFor="password"
-                      >
-                        Sudo password
-                      </label>
-                      <div className="relative">
-                        <input
-                          id="password"
-                          value={password}
-                          onChange={(e) => setPassword(e.target.value)}
-                          type={showPw ? 'text' : 'password'}
-                          minLength={16}
-                          required
-                          autoFocus
-                          className="w-full "
-                        />
-                        <button
-                          className="absolute inset-y-0 right-4"
-                          type="button"
-                          onClick={() => setShowPw(!showPw)}
-                          tabIndex={-1}
+                    <div className="py-4">
+                      <p className="text-neutral-500">
+                        Please enter your <code>sudo</code> password to unlock the user keyring.
+                        This is required for data to be decrypted on this screen.
+                      </p>
+                    </div>
+                    <div className="flex justify-between items-end gap-4">
+                      <div className="space-y-4 max-w-lg">
+                        <label
+                          className="block text-gray-700 text-sm font-bold mb-2"
+                          htmlFor="password"
                         >
-                          {showPw ? <FaEyeSlash /> : <FaEye />}
-                        </button>
+                          Sudo password
+                        </label>
+                        <div className="relative">
+                          <input
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            type={showPw ? 'text' : 'password'}
+                            minLength={16}
+                            required
+                            autoFocus
+                            className="w-full "
+                          />
+                          <button
+                            className="absolute inset-y-0 right-4"
+                            type="button"
+                            onClick={() => setShowPw(!showPw)}
+                            tabIndex={-1}
+                          >
+                            {showPw ? <FaEyeSlash /> : <FaEye />}
+                          </button>
+                        </div>
                       </div>
                       <div>
                         <Button type="submit" variant="primary">
