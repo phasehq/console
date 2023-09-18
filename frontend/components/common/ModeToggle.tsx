04a2ab205256e01c36381fb86399a1dd71555bf5
rohan-chaturvedi <rohan.chaturvedi@protonmail.com>
2023-09-18 16:12:12 +0530

    feat: improve mode toggle styling

--- frontend/components/common/ModeToggle.tsx
+++ frontend/components/common/ModeToggle.tsx
@@ -20,21 +20,15 @@ export function ModeToggle() {
         checked={isDark}
         onChange={toggleTheme}
         className={`${
-          isDark ? 'bg-emerald-700' : 'bg-neutral-500/40'
-        } relative inline-flex h-6 w-11 items-center rounded-full`}
+          isDark ? 'bg-emerald-400/10 ring-emerald-400/20' : 'bg-neutral-500/40 ring-neutral-500/30'
+        } relative inline-flex h-6 w-11 items-center rounded-full ring-1 ring-inset`}
       >
         <span className="sr-only">Enable dark theme</span>
         <span
           className={`${
-            isDark ? 'translate-x-6 bg-white' : 'translate-x-1 bg-black'
+            isDark ? 'translate-x-6 bg-emerald-400' : 'translate-x-1 bg-black'
           } flex items-center justify-center h-4 w-4 transform rounded-full transition`}
-        >
-          {/* {isDark ? (
-            <FaMoon className="text-black h-3 w-3" />
-          ) : (
-            <FaSun className="text-white h-3 w-3" />
-          )} */}
-        </span>
+        ></span>
       </Switch>
     </div>
   )
