a3c592b479b4d95e48ab3cccc8eeeab0709942fe
Rohan Chaturvedi <rohan.chaturvedi@protonmail.com>
2023-10-09 12:57:19 +0530

    fix: handle no secret history for new secret (#66)

--- frontend/components/environments/SecretRow.tsx
+++ frontend/components/environments/SecretRow.tsx
@@ -261,7 +261,7 @@ const HistoryDialog = (props: { secret: SecretType }) => {
     if (eventType === ApiSecretEventEventTypeChoices.D) return 'Deleted'
   }
 
-  const secretHistory = secret.history!.filter(
+  const secretHistory = secret.history?.filter(
     (event: Maybe<SecretEventType>) => event?.eventType! !== ApiSecretEventEventTypeChoices.R
   )
 
