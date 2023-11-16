7f328bfe6202ed2a14c6c65f8c248cdf49aa3631
rohan-chaturvedi <rohan.chaturvedi@protonmail.com>
2023-11-16 12:51:10 +0530

    feat: show red spinner on danger buttons when isLoading

--- frontend/components/common/Spinner.tsx
+++ frontend/components/common/Spinner.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx'
+import { color } from 'framer-motion'
 
 const SIZES = {
   sm: 'border-2 h-4 w-4',
@@ -7,16 +8,26 @@ const SIZES = {
   xl: 'border-6 h-12 w-12',
 }
 
-interface SpinnerProps {
-  size: 'sm' | 'md' | 'lg' | 'xl'
+const COLORS = {
+  emerald: 'border-emerald-500',
+  red: 'border-red-500',
 }
 
-export default function Spinner(props: SpinnerProps = { size: 'lg' }) {
-  const { size } = props
+interface SpinnerProps {
+  size: 'sm' | 'md' | 'lg' | 'xl'
+  color?: 'emerald' | 'red'
+}
+
+export default function Spinner(props: SpinnerProps = { size: 'lg', color: 'emerald' }) {
+  const { size, color } = props
+
+  const spinnerColor = color || 'emerald'
+
   const BASE_STYLE =
-    'inline-block animate-spin rounded-full border-4 border-solid border-emerald-500 border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]'
+    'inline-block animate-spin rounded-full border-4 border-solid border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]'
+
   return (
-    <div className={clsx(BASE_STYLE, SIZES[size])} role="status">
+    <div className={clsx(BASE_STYLE, SIZES[size], COLORS[spinnerColor])} role="status">
       <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
         Loading...
       </span>
