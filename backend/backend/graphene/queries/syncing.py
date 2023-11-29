3fdc436ab0222e274942646e117de0a0e9c3cc1a
rohan-chaturvedi <rohan.chaturvedi@protonmail.com>
2023-11-29 14:42:49 +0530

    feat: misc updates to sync queries

--- backend/backend/graphene/queries/syncing.py
+++ backend/backend/graphene/queries/syncing.py
@@ -1,6 +1,6 @@
-from api.crypto import encrypt_asymmetric, get_server_keypair
-from api.services import get_service_config
+from api.crypto import decrypt_asymmetric, encrypt_asymmetric, get_server_keypair
 from api.models import Environment, EnvironmentSync, ServerEnvironmentKey
+from backend.graphene.utils.syncing.cloudflare.pages import list_cloudflare_pages
 from backend.graphene.utils.permissions import user_can_access_app
 from graphql import GraphQLError
 
@@ -11,41 +11,31 @@ def resolve_server_public_key(root, info):
 
 
 def resolve_sync_enabled(root, info, app_id):
-
     # ServerEnvironmentKey.objects.all().delete()
 
     if not user_can_access_app(info.context.user.userId, app_id):
         raise GraphQLError("You don't have access to this app")
 
-    app_envs = Environment.objects.filter(app_id=app_id).values_list('id')
+    app_envs = Environment.objects.filter(app_id=app_id).values_list("id")
     return ServerEnvironmentKey.objects.filter(environment_id__in=app_envs).exists()
 
 
-def create_cloudflare_sync(root, info, env_id, project, project_env, access_token, account_id):
-    service_config = get_service_config('cloudflare')
+def resolve_cloudflare_pages_projects(root, info, account_id, access_token):
+    pk, sk = get_server_keypair()
+    decrypted_account_id = decrypt_asymmetric(account_id, sk.hex(), pk.hex())
+    decrypted_access_token = decrypt_asymmetric(access_token, sk.hex(), pk.hex())
 
-    env = Environment.objects.get(id=env_id)
+    try:
+        projects = list_cloudflare_pages(decrypted_account_id, decrypted_access_token)
+        return projects
+    except Exception as ex:
+        raise GraphQLError(ex)
 
-    if not ServerEnvironmentKey.objects.filter(environment=env).exists():
-        raise GraphQLError("Syncing is not enabled for this environment!")
 
-    if not user_can_access_app(info.context.user.userId, env.app.id):
+def resolve_app_syncs(root, info, app_id):
+    if not user_can_access_app(info.context.user.userId, app_id):
         raise GraphQLError("You don't have access to this app")
 
-    pk, sk = get_server_keypair()
+    # EnvironmentSync.objects.all().delete()
 
-    wrapped_account_id = encrypt_asymmetric(account_id, pk.hex())
-    wrapped_access_token = encrypt_asymmetric(access_token, pk.hex())
-
-    sync_options = {
-        "project": project,
-        "environment": project_env
-    }
-
-    authentication_credentials = {
-        "access_token": wrapped_access_token,
-        "account_id": wrapped_account_id
-    }
-
-    EnvironmentSync.objects.create(environment=env, service='cloudflare',
-                                   options=sync_options, authentication=authentication_credentials)
+    return EnvironmentSync.objects.filter(environment__app__id=app_id)
