73a7e1634c8ecc040aed34eba5eeda102dd41013
Nimish <nimish@phase.dev>
2024-01-02 14:56:03 +0530

    feat: switched to jsdom; added module import context in jest config

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,10 +1,14 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   roots: ['<rootDir>/tests'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1"
+  },
+  
 };
