2fe99a417030757ba486ef377da85fab1671b889
rohan-chaturvedi <rohan.chaturvedi@protonmail.com>
2024-01-09 22:34:34 +0530

    fix: remove refresh token logic

--- backend/api/utils/syncing/auth.py
+++ backend/api/utils/syncing/auth.py
@@ -3,20 +3,18 @@ from api.services import Providers
 from django.apps import apps
 
 
-def store_oauth_token(provider_id, access_token, refresh_token, org_id):
+def store_oauth_token(provider_id, access_token, org_id):
     Organisation = apps.get_model("api", "Organisation")
     ProviderCredentials = apps.get_model("api", "ProviderCredentials")
 
     pk, _ = get_server_keypair()
 
     encrypted_access_token = encrypt_asymmetric(access_token, pk.hex())
-    encrypted_refresh_token = encrypt_asymmetric(refresh_token, pk.hex())
 
     provider = Providers.get_provider_config(provider_id)
 
     credentials = {
         "access_token": encrypted_access_token,
-        "refresh_token": encrypted_refresh_token,
     }
 
     org = Organisation.objects.get(id=org_id)
