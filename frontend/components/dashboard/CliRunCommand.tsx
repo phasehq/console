6bb44be7124c1e9c4ac97c5dcd0080e5e5fe1713
Nimish <nimish@phase.dev>
2024-02-01 23:16:49 +0530

    feat: improved run command codebox

--- frontend/components/dashboard/CliRunCommand.tsx
+++ frontend/components/dashboard/CliRunCommand.tsx
@@ -57,12 +57,12 @@ function CopyButton(props: { code: string }) {
 }
 
 export const CliRunCommand = () => {
-  const command = 'phase run /* YOUR APPLICATIONS START COMMAND */'
+  const command = 'phase run'
 
   return (
     <div className="group relative overflow-x-auto rounded-lg border border-neutral-500/40 bg-zinc-900/50 p-4 text-left text-sm text-zinc-100">
       <pre>
-        <span className="text-violet-300">phase run</span> /* YOUR APPLICATIONS START COMMAND */
+        <span className="text-violet-300">phase</span> run <span className="text-[#FF8700]">{"YOUR_APPLICATION_START_COMMAND"}</span>
       </pre>
       <CopyButton code={command} />
     </div>
