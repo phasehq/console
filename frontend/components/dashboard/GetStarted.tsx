8b4772b795c9344a59df63835ba584082573f292
rohan-chaturvedi <rohan.chaturvedi@protonmail.com>
2024-02-01 20:09:39 +0530

    feat: add resources sidebar

--- frontend/components/dashboard/GetStarted.tsx
+++ frontend/components/dashboard/GetStarted.tsx
@@ -2,12 +2,17 @@ import { Disclosure, Menu, Transition } from '@headlessui/react'
 import clsx from 'clsx'
 import { Fragment, ReactNode, useEffect, useState } from 'react'
 import {
+  FaArrowRight,
+  FaBoxOpen,
   FaCheckCircle,
   FaChevronRight,
+  FaDesktop,
+  FaMagic,
   FaMinusCircle,
   FaQuestionCircle,
   FaRegCircle,
   FaRegDotCircle,
+  FaTerminal,
   FaTimesCircle,
 } from 'react-icons/fa'
 
@@ -22,6 +27,8 @@ import { CliInitCommand } from './CliInitCommand'
 import { CliRunCommand } from './CliRunCommand'
 import { RoleLabel } from '../users/RoleLabel'
 import Spinner from '../common/Spinner'
+import { Card } from '../common/Card'
+import { SiGithub, SiSlack, SiX } from 'react-icons/si'
 
 const TaskPanel = (props: {
   title: string
@@ -227,6 +234,39 @@ export const GetStarted = (props: { organisation: OrganisationType }) => {
     )
   }
 
+  const resources = [
+    {
+      href: 'https://docs.phase.dev/quickstart',
+      title: 'Quickstart',
+      description: 'A step-by-step guide on getting up and running with Phase',
+      logo: <FaMagic className="shrink-0" />,
+    },
+    {
+      href: 'https://docs.phase.dev/console',
+      title: 'Console',
+      description: 'Complete documentation for the Phase Console',
+      logo: <FaDesktop className="shrink-0" />,
+    },
+    {
+      href: 'https://docs.phase.dev/cli/commands',
+      title: 'CLI',
+      description: 'Complete documentation for the Phase CLI',
+      logo: <FaTerminal className="shrink-0" />,
+    },
+    {
+      href: 'https://docs.phase.dev/integrations',
+      title: 'Framework Integrations',
+      description: 'Learn how to inject secrets into your application runtime',
+      logo: <FaBoxOpen className="shrink-0" />,
+    },
+    {
+      href: 'https://slack.phase.dev',
+      title: 'Join Slack',
+      description: 'Join the Phase community on Slack',
+      logo: <SiSlack className="shrink-0" />,
+    },
+  ]
+
   if (!showGuide)
     return (
       <div className="flex justify-end">
@@ -237,19 +277,20 @@ export const GetStarted = (props: { organisation: OrganisationType }) => {
     )
 
   return (
-    <div className="space-y-6">
-      <div className="flex items-start justify-between">
-        <div>
-          <h1 className="text-black dark:text-white font-semibold text-2xl">Getting started</h1>
-          <p className="text-neutral-500">
-            Learn how to start using the Phase Console by creating an App, setting up your local dev
-            environment, adding your team members and setting up a third party integration.
-          </p>
+    <div className="flex gap-6">
+      <div className="space-y-6 w-2/3">
+        <div className="flex items-start justify-between">
+          <div>
+            <h1 className="text-black dark:text-white font-semibold text-2xl">Getting started</h1>
+            <p className="text-neutral-500">
+              Learn how to start using the Phase Console by creating an App, setting up your local
+              dev environment, adding your team members and setting up a third party integration.
+            </p>
+          </div>
+          <div>
+            <DismissMenu />
+          </div>
         </div>
-        <div>
-          <DismissMenu />
-        </div>
-      </div>
 
       {loading ? (
         <div className="p-40 flex items-center justify-center">
@@ -382,42 +423,42 @@ export const GetStarted = (props: { organisation: OrganisationType }) => {
                 <li>Invite team members to collaborate with you and securely share secrets.</li>
               </ul>
 
-              <div className="space-y-2">
-                <div
-                  className={clsx(
-                    'flex items-center gap-2 text-sm',
-                    memberInvited ? 'text-emerald-500' : 'text-neutral-500'
-                  )}
-                >
-                  {memberInvited ? <FaCheckCircle /> : <FaRegCircle />}
-                  Invite a team member
-                </div>
-                <div
-                  className={clsx(
-                    'flex items-center gap-2 text-sm',
-                    memberAdded ? 'text-emerald-500' : 'text-neutral-500'
-                  )}
-                >
-                  {memberAdded ? <FaCheckCircle /> : <FaRegCircle />}
-                  Member joined Organisation
-                </div>
-              </div>
-
-              {!memberAdded && (
-                <div className="flex gap-4">
-                  <Link href={`/${organisation.name}/members`}>
-                    <Button variant="primary">Go to Members</Button>
-                  </Link>
-                  <Link
-                    href="https://docs.phase.dev/console/users#add-users-to-an-organisation"
-                    target="_blank"
+                <div className="space-y-2">
+                  <div
+                    className={clsx(
+                      'flex items-center gap-2 text-sm',
+                      memberInvited ? 'text-emerald-500' : 'text-neutral-500'
+                    )}
                   >
-                    <Button variant="secondary">View Docs</Button>
-                  </Link>
+                    {memberInvited ? <FaCheckCircle /> : <FaRegCircle />}
+                    Invite a team member
+                  </div>
+                  <div
+                    className={clsx(
+                      'flex items-center gap-2 text-sm',
+                      memberAdded ? 'text-emerald-500' : 'text-neutral-500'
+                    )}
+                  >
+                    {memberAdded ? <FaCheckCircle /> : <FaRegCircle />}
+                    Member joined Organisation
+                  </div>
                 </div>
-              )}
-            </div>
-          </TaskPanel>
+
+                {!memberAdded && (
+                  <div className="flex gap-4">
+                    <Link href={`/${organisation.name}/members`}>
+                      <Button variant="primary">Go to Members</Button>
+                    </Link>
+                    <Link
+                      href="https://docs.phase.dev/console/users#add-users-to-an-organisation"
+                      target="_blank"
+                    >
+                      <Button variant="secondary">View Docs</Button>
+                    </Link>
+                  </div>
+                )}
+              </div>
+            </TaskPanel>
 
           <TaskPanel
             title="Set up an Integration"
@@ -462,21 +503,66 @@ export const GetStarted = (props: { organisation: OrganisationType }) => {
                 </div>
               </div>
 
-              <div className="flex gap-4">
-                <Link href={`/${organisation.name}/integrations`}>
-                  <Button variant="primary">Go to Integrations</Button>
-                </Link>
-                <Link
-                  href="https://docs.phase.dev/console/users#add-users-to-an-organisation"
-                  target="_blank"
-                >
-                  <Button variant="secondary">View Docs</Button>
-                </Link>
+                <div className="flex gap-4">
+                  <Link href={`/${organisation.name}/integrations`}>
+                    <Button variant="primary">Go to Integrations</Button>
+                  </Link>
+                  <Link
+                    href="https://docs.phase.dev/console/users#add-users-to-an-organisation"
+                    target="_blank"
+                  >
+                    <Button variant="secondary">View Docs</Button>
+                  </Link>
+                </div>
               </div>
-            </div>
-          </TaskPanel>
+            </TaskPanel>
+          </div>
+        )}
+      </div>
+      <div className="w-1/3 pl-4 border-l border-neutral-500/40 space-y-6">
+        <div>
+          <h1 className="text-black dark:text-white font-semibold text-2xl">Resources</h1>
+          <p className="text-neutral-500">
+            Need more help? Here are some more resources to help you get started with Phase.
+          </p>
         </div>
-      )}
+
+        <div className="flex flex-col gap-4">
+          {resources.map((resource) => (
+            <Card key={resource.title}>
+              <Link href={resource.href} target="_blank" className="flex flex-row-reverse gap-6">
+                <div className="flex-auto">
+                  <h3 className=" font-semibold text-zinc-900 dark:text-white">{resource.title}</h3>
+                  <p className="mt-1 text-sm text-zinc-600 dark:text-zinc-400">
+                    {resource.description}
+                  </p>
+                  <div className="mt-4 flex items-center gap-1 text-sm">
+                    <div className="flex items-center text-emerald-500">Explore</div>
+                    <FaArrowRight className="text-emerald-500 text-xs" />
+                  </div>
+                </div>
+                <div className="text-3xl">{resource.logo}</div>
+              </Link>
+            </Card>
+          ))}
+        </div>
+
+        <div className="pt-4 border-t border-neutral-500/40">
+          <div className="flex items-center gap-4 justify-end">
+            <Link href="https://slack.phase.dev">
+              <SiSlack className="text-neutral-500 hover:text-neutral-600" />
+            </Link>
+
+            <Link href="https://github.com/phasehq">
+              <SiGithub className="text-neutral-500 hover:text-neutral-600" />
+            </Link>
+
+            <Link href="https://twitter.com/phasedotdev">
+              <SiX className="text-neutral-500 hover:text-neutral-600" />
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
