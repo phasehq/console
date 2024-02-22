f052680b84b06b407ca3238efecc4781e6e956ac
rohan-chaturvedi <rohan.chaturvedi@protonmail.com>
2024-02-22 14:15:55 +0530

    refactor: misc updates to keyring dialog

--- frontend/components/auth/UnlockKeyringDialog.tsx
+++ frontend/components/auth/UnlockKeyringDialog.tsx
@@ -1,6 +1,17 @@
+'use client'
+
 import { Dialog, Transition } from '@headlessui/react'
-import { Fragment, useContext, useEffect, useState } from 'react'
-import { FaEye, FaEyeSlash, FaLock, FaTimes, FaUnlock } from 'react-icons/fa'
+import { Fragment, useContext, useEffect, useRef, useState } from 'react'
+import {
+  FaCheckCircle,
+  FaCircle,
+  FaEye,
+  FaEyeSlash,
+  FaLock,
+  FaShieldAlt,
+  FaSignOutAlt,
+  FaUnlock,
+} from 'react-icons/fa'
 import { Button } from '../common/Button'
 import { KeyringContext } from '@/contexts/keyringContext'
 import { cryptoUtils } from '@/utils/auth'
@@ -12,18 +23,60 @@ import { RoleLabel } from '../users/RoleLabel'
 import { Avatar } from '../common/Avatar'
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
+import { handleSignout } from '@/apollo/client'
+import { SplitButton } from '../common/SplitButton'
+import { getDevicePassword, setDevicePassword } from '@/utils/localStorage'
 
 export default function UnlockKeyringDialog(props: { organisation: OrganisationType }) {
   const { organisation } = props
 
   const [password, setPassword] = useState<string>('')
   const [showPw, setShowPw] = useState<boolean>(false)
+
+  const [trustDevice, setTrustDevice] = useState(false)
+
   const [isOpen, setIsOpen] = useState<boolean>(false)
   const [unlocking, setUnlocking] = useState(false)
 
   const { keyring, setKeyring } = useContext(KeyringContext)
+
   const { data: session } = useSession()
   const pathname = usePathname()
+
+  let inputRef = useRef(null)
+
+  const reset = () => {
+    setPassword('')
+    setShowPw(false)
+    setTrustDevice(false)
+  }
+
+  const decryptKeyring = (sudoPassword: string) => {
+    return new Promise(async (resolve, reject) => {
+      setUnlocking(true)
+
+      try {
+        if (trustDevice) {
+          setDevicePassword(organisation.memberId!, sudoPassword)
+        }
+
+        const decryptedKeyring = await cryptoUtils.getKeyring(
+          session?.user?.email!,
+          organisation,
+          sudoPassword
+        )
+        setKeyring(decryptedKeyring)
+        setUnlocking(false)
+        reset()
+        closeModal()
+        resolve(true) // Resolve the promise successfully
+      } catch (e) {
+        console.error(e)
+        setUnlocking(false)
+        reject(e) // Reject the promise with the error
+      }
+    })
+  }
 
   useEffect(() => {
     const IGNORE_PATHS = ['recovery']
@@ -37,6 +90,18 @@ export default function UnlockKeyringDialog(props: { organisation: OrganisationT
     }
   }, [keyring, pathname])
 
+  useEffect(() => {
+    if (organisation) reset()
+
+    const devicePassword = getDevicePassword(organisation.memberId!)
+
+    if (devicePassword) {
+      setPassword(devicePassword)
+      decryptKeyring(devicePassword)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [organisation])
+
   const closeModal = () => {
     setIsOpen(false)
   }
@@ -45,32 +110,30 @@ export default function UnlockKeyringDialog(props: { organisation: OrganisationT
     setIsOpen(true)
   }
 
-  const decryptKeyring = async (event: { preventDefault: () => void }) => {
+  const handleFormSubmit = async (event: { preventDefault: () => void }) => {
     event.preventDefault()
-    setUnlocking(true)
-
-    try {
-      const decryptedKeyring = await cryptoUtils.getKeyring(
-        session?.user?.email!,
-        organisation,
-        password
-      )
-      setKeyring(decryptedKeyring)
-      toast.success('Unlocked user keyring.', { autoClose: 2000 })
-      setUnlocking(false)
-      closeModal()
-    } catch (e) {
-      console.error(e)
-      setUnlocking(false)
-      toast.error('Failed to decrypt keys. Please verify your sudo password and try again.')
-      return false
-    }
+    toast.promise(decryptKeyring(password), {
+      pending: 'Unlocking...',
+      success: {
+        render() {
+          return 'Unlocked user keyring!'
+        },
+        autoClose: 2000,
+      },
+      error: {
+        render() {
+          return 'Something went wrong! Please check your sudo password and try again.'
+        },
+        autoClose: 2000,
+      },
+    })
+    decryptKeyring(password)
   }
 
   return (
     <>
       <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={() => {}}>
+        <Dialog as="div" className="relative z-10" onClose={() => {}} initialFocus={inputRef}>
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -106,7 +169,7 @@ export default function UnlockKeyringDialog(props: { organisation: OrganisationT
                       Unlock User Keyring
                     </h3>
                   </Dialog.Title>
-                  <form onSubmit={decryptKeyring}>
+                  <form onSubmit={handleFormSubmit}>
                     <div className="py-4">
                       <p className="text-neutral-500">
                         Please enter your <code>sudo</code> password to unlock the user keyring.
@@ -114,29 +177,40 @@ export default function UnlockKeyringDialog(props: { organisation: OrganisationT
                       </p>
                     </div>
 
-                    <div className="ring-1 ring-inset ring-neutral-500/40 p-4 rounded-lg bg-zinc-200 dark:bg-zinc-800 space-y-4">
-                      <div className="flex items-center justify-between">
-                        <div className="whitespace-nowrap flex items-center gap-2">
-                          <Avatar imagePath={session?.user?.image!} size="md" />
-                          <div className="flex flex-col gap-2">
-                            <div className="flex flex-col">
-                              <span className="text-sm font-medium text-black dark:text-white">
-                                {session?.user?.name}
-                              </span>
-                              <span className="text-neutral-500 text-2xs">
-                                {session?.user?.email}
-                              </span>
+                    <div className="ring-1 ring-inset ring-neutral-500/40 shadow-lg p-4 rounded-lg bg-zinc-200 dark:bg-zinc-800 space-y-4">
+                      <div className="flex justify-between">
+                        <div className="flex flex-col gap-4">
+                          <div className="whitespace-nowrap flex items-start gap-2">
+                            <div className="pt-2">
+                              <Avatar imagePath={session?.user?.image!} size="md" />
+                            </div>
+                            <div className="flex flex-col gap-2">
+                              <div className="flex flex-col">
+                                <span className="text-sm font-medium text-black dark:text-white">
+                                  {session?.user?.name}
+                                </span>
+                                <span className="text-neutral-500 text-2xs">
+                                  {session?.user?.email}
+                                </span>
+                              </div>
+                              <div className="flex items-center gap-2 text-xs">
+                                <h2 className="font-semibold text-black dark:text-white">
+                                  {organisation.name}
+                                </h2>
+                                <span className="text-neutral-500">
+                                  <RoleLabel role={organisation.role!} />
+                                </span>
+                              </div>
                             </div>
                           </div>
                         </div>
 
-                        <div className="flex items-center gap-2">
-                          <h2 className="text-xl font-semibold text-black dark:text-white">
-                            {organisation.name}
-                          </h2>
-                          <span className="text-neutral-500">
-                            <RoleLabel role={organisation.role!} />
-                          </span>
+                        <div>
+                          <Button type="button" variant="outline" onClick={() => handleSignout()}>
+                            <div className="flex items-center gap-1 text-xs">
+                              <FaSignOutAlt /> Log out
+                            </div>
+                          </Button>
                         </div>
                       </div>
 
@@ -151,6 +225,8 @@ export default function UnlockKeyringDialog(props: { organisation: OrganisationT
                           <div className="flex justify-between w-full bg-zinc-100 dark:bg-zinc-800 ring-1 ring-inset ring-neutral-500/40 roudned-md focus-within:ring-1 focus-within:ring-inset focus-within:ring-emerald-500 rounded-md p-px">
                             <input
                               id="password"
+                              ref={inputRef}
+                              tabIndex={0}
                               value={password}
                               onChange={(e) => setPassword(e.target.value)}
                               type={showPw ? 'text' : 'password'}
@@ -170,18 +246,50 @@ export default function UnlockKeyringDialog(props: { organisation: OrganisationT
                           </div>
                         </div>
                         <div>
-                          <Button type="submit" variant="primary" isLoading={unlocking}>
+                          {/* <Button type="submit" variant="primary" isLoading={unlocking}>
                             <FaUnlock /> Unlock
-                          </Button>
+                          </Button> */}
+                          <SplitButton
+                            type="submit"
+                            variant="primary"
+                            isLoading={unlocking}
+                            menuContent={
+                              <div className="w-40">
+                                <Button
+                                  type="button"
+                                  variant="secondary"
+                                  isLoading={unlocking}
+                                  onClick={() => setTrustDevice(!trustDevice)}
+                                >
+                                  <div
+                                    className={clsx(
+                                      'flex items-center gap-2',
+                                      trustDevice ? 'text-emerald-500' : 'text-neutral-500'
+                                    )}
+                                  >
+                                    {trustDevice ? <FaCheckCircle /> : <FaCircle />} Trust this
+                                    device
+                                  </div>
+                                </Button>
+                              </div>
+                            }
+                          >
+                            {trustDevice ? (
+                              <FaShieldAlt className="shrink-0" />
+                            ) : (
+                              <FaUnlock className="shrink-0" />
+                            )}{' '}
+                            {trustDevice ? 'Trust' : 'Unlock'}
+                          </SplitButton>
                         </div>
                       </div>
 
-                      <div className="flex">
+                      <div className="flex items-center justify-between border-t border-neutral-500/20 pt-2">
                         <Link
                           className="text-xs text-neutral-500 hover:text-black dark:hover:text-white transition ease"
                           href={`/${organisation.name}/recovery`}
                         >
-                          Forgot password
+                          Reset password
                         </Link>
                       </div>
                     </div>
