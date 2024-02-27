50a717cca1c8071d9e0b275e70e905baf8d2f4d5
Rohan Chaturvedi <rohan.chaturvedi@protonmail.com>
2024-02-27 10:44:14 +0530

    fix: validate host and key before intializing posthog (#198)

--- frontend/utils/posthog.ts
+++ frontend/utils/posthog.ts
@@ -1,7 +1,11 @@
 import posthog from 'posthog-js'
 
 export function initializePostHog() {
-  if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+  if (
+    typeof window !== 'undefined' &&
+    process.env.NEXT_PUBLIC_POSTHOG_KEY &&
+    process.env.NEXT_PUBLIC_POSTHOG_HOST !== 'BAKED_NEXT_PUBLIC_POSTHOG_HOST'
+  ) {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
       capture_pageview: true,
