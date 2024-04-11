b5829d3882a5581510240e6afae587fb98997a61
rohan-chaturvedi <rohan.chaturvedi@protonmail.com>
2024-04-11 11:07:39 +0530

    feat: add utils to get current hostname

--- frontend/utils/appConfig.ts
+++ frontend/utils/appConfig.ts
@@ -3,3 +3,10 @@ export const isCloudHosted = () => {
     process.env.NEXT_PUBLIC_APP_HOST?.trim() === 'cloud' || process.env.APP_HOST?.trim() === 'cloud'
   )
 }
+
+export const hostname = `${window.location.protocol}//${window.location.host}`
+
+export const apiHost =
+  window.location.host === 'cloud.phase.dev'
+    ? 'https://api.phase.dev'
+    : `${hostname}/service/public`
