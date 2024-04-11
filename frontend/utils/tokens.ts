48b3ecfc7d5896054406aa27ef452d5fb00a9b6d
rohan-chaturvedi <rohan.chaturvedi@protonmail.com>
2024-04-11 11:07:53 +0530

    feat: add token expiry utils

---
+++ frontend/utils/tokens.ts
@@ -0,0 +1,34 @@
+import { getUnixTimeStampinFuture } from './time'
+
+export interface ExpiryOptionT {
+  name: string
+  getExpiry: () => number | null
+}
+
+export const tokenExpiryOptions: ExpiryOptionT[] = [
+  {
+    name: 'Never',
+    getExpiry: () => null,
+  },
+  {
+    name: '7 days',
+    getExpiry: () => getUnixTimeStampinFuture(7),
+  },
+  {
+    name: '30 days',
+    getExpiry: () => getUnixTimeStampinFuture(30),
+  },
+  {
+    name: '60 days',
+    getExpiry: () => getUnixTimeStampinFuture(60),
+  },
+  {
+    name: '90 days',
+    getExpiry: () => getUnixTimeStampinFuture(90),
+  },
+]
+
+export const humanReadableExpiry = (expiryOption: ExpiryOptionT) =>
+  expiryOption.getExpiry() === null
+    ? 'This token will never expire.'
+    : `This token will expire on ${new Date(expiryOption.getExpiry()!).toLocaleDateString()}.`
