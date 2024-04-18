43c31d46450df2090f99d62138be4f7adb833baa
rohan-chaturvedi <rohan.chaturvedi@protonmail.com>
2024-04-18 19:48:09 +0530

    feat: add permalink section to share secret dialog

--- frontend/components/environments/secrets/ShareSecretDialog.tsx
+++ frontend/components/environments/secrets/ShareSecretDialog.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/common/Button'
 import GenericDialog from '@/components/common/GenericDialog'
 import { CreateSharedSecret } from '@/graphql/mutations/environments/shareSecret.gql'
 import { useMutation } from '@apollo/client'
-import { Fragment, useState } from 'react'
+import { Fragment, useContext, useState } from 'react'
 import { FaChevronDown, FaCircle, FaDotCircle, FaInfo, FaShare, FaShareAlt } from 'react-icons/fa'
 import _sodium from 'libsodium-wrappers-sumo'
 import { getUnixTimeStampinFuture } from '@/utils/time'
@@ -12,6 +12,9 @@ import clsx from 'clsx'
 import { toast } from 'react-toastify'
 import CopyButton from '@/components/common/CopyButton'
 import { boxExpiryString, encryptBox, newBoxSeed } from '@/utils/lockbox'
+import { getSecretPermalink } from '@/utils/secrets'
+import { organisationContext } from '@/contexts/organisationContext'
+import { getHostname } from '@/utils/appConfig'
 
 interface ExpiryOptionT {
   name: string
@@ -48,6 +51,8 @@ const compareExpiryOptions = (a: ExpiryOptionT, b: ExpiryOptionT) => {
 }
 
 export const ShareSecretDialog = (props: { secret: SecretType }) => {
+  const { activeOrganisation: organisation } = useContext(organisationContext)
+
   const [createLockbox, { error, loading }] = useMutation(CreateSharedSecret)
 
   const [secretData, setSecretData] = useState({ text: props.secret.value })
@@ -55,6 +60,10 @@ export const ShareSecretDialog = (props: { secret: SecretType }) => {
   const [expiry, setExpiry] = useState<ExpiryOptionT>(lockboxExpiryOptions[1])
 
   const [box, setBox] = useState<{ lockboxId: string; password: string } | null>(null)
+
+  const permalink = organisation
+    ? `${getHostname()}${getSecretPermalink(props.secret, organisation.name)}`
+    : ''
 
   const reset = () => {
     setSecretData({ text: props.secret.value })
@@ -107,7 +116,7 @@ export const ShareSecretDialog = (props: { secret: SecretType }) => {
 
   return (
     <GenericDialog
-      title={box ? 'Share this link' : 'Share secret'}
+      title={box ? 'Share this link' : `Share ${props.secret.key}`}
       buttonVariant="outline"
       buttonContent={
         <span className="py-1">
@@ -131,104 +140,129 @@ export const ShareSecretDialog = (props: { secret: SecretType }) => {
           </div>
         </div>
       ) : (
-        <div className="space-y-6">
-          <div className="text-neutral-500">
-            Create a link to share this secret with zero-trust encryption
+        <div className="space-y-6 divide-y divide-neutral-500/40 py-4">
+          <div className="space-y-2">
+            <div>
+              <div className="font-semibold text-black dark:text-white">Permalink</div>
+              <div className="text-neutral-500 text-sm">
+                Get a permalink to this secret to share with your team on Phase
+              </div>
+            </div>
+            <div className="group relative overflow-x-hidden rounded-lg border border-neutral-500/40 bg-zinc-300/50 dark:bg-zinc-800/50 p-3 text-left text-sm text-emerald-800 dark:text-emerald-300">
+              <pre className="text-xs ph-no-capture">{permalink}</pre>
+              <div className="absolute inset-0 w-full h-full bg-gradient-to-r from-transparent to-zinc-300 dark:to-zinc-800"></div>
+              <div className="absolute right-1 top-2.5 ">
+                <CopyButton value={permalink} />
+              </div>
+            </div>
           </div>
 
-          <form className="space-y-6" onSubmit={handleSubmit}>
-            <div className="space-y-2 w-full">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="secret">
-                Secret
-              </label>
-              <textarea
-                rows={5}
-                maxLength={10000}
-                value={secretData.text}
-                className="w-full ph-no-capture"
-                onChange={(e) => handleTextChange(e.target.value)}
-              ></textarea>
+          <div className="space-y-2 pt-4">
+            <div>
+              <div className="font-semibold text-black dark:text-white">One-time share</div>
+              <div className="text-neutral-500 text-sm">
+                Create a link to share this secret with zero-trust encryption
+              </div>
             </div>
 
-            <div className="grid grid-cols-3 gap-8">
-              <div className="col-span-2">
-                <RadioGroup value={expiry} by={compareExpiryOptions} onChange={setExpiry}>
-                  <RadioGroup.Label as={Fragment}>
-                    <label className="block text-gray-700 text-sm font-bold mb-2">Expiry</label>
-                  </RadioGroup.Label>
-                  <div className="flex flex-wrap items-center gap-3">
-                    {lockboxExpiryOptions.map((option) => (
-                      <RadioGroup.Option key={option.name} value={option} as={Fragment}>
-                        {({ active, checked }) => (
-                          <div
-                            className={clsx(
-                              'flex items-center gap-2 py-0.5 px-2 cursor-pointer bg-zinc-800 border border-zinc-800  rounded-full text-sm',
-                              active && 'border-zinc-700',
-                              checked && 'bg-zinc-700'
-                            )}
-                          >
-                            {checked ? <FaDotCircle className="text-emerald-500" /> : <FaCircle />}
-                            {option.name}
-                          </div>
-                        )}
-                      </RadioGroup.Option>
-                    ))}
-                  </div>
-                </RadioGroup>
-              </div>
-
-              <div className="relative">
+            <form className="space-y-6" onSubmit={handleSubmit}>
+              <div className="space-y-2 w-full">
                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="secret">
-                  Maximum allowed views
+                  Secret
                 </label>
-                <Listbox value={allowedViews} onChange={setAllowedViews}>
-                  {({ open }) => (
-                    <>
-                      <Listbox.Button as={Fragment} aria-required>
-                        <div className="p-2 flex items-center justify-between  rounded-md h-10 cursor-pointer bg-zinc-200 dark:bg-zinc-800 text-zinc-900 dark:text-zinc-100">
-                          {allowedViews || 'Unlimited'}
-                          <FaChevronDown
-                            className={clsx(
-                              'transition-transform ease duration-300 text-neutral-500',
-                              open ? 'rotate-180' : 'rotate-0'
-                            )}
-                          />
-                        </div>
-                      </Listbox.Button>
-                      <Listbox.Options>
-                        <div className="bg-zinc-300 dark:bg-zinc-800 p-2 rounded-md shadow-2xl absolute mt-1 z-20 ring-1 ring-inset ring-neutral-500/40 w-full">
-                          {lockboxAllowedViews.map((num: number | undefined) => (
-                            <Listbox.Option key={num} value={num} as={Fragment}>
-                              {({ active, selected }) => (
-                                <div
-                                  className={clsx(
-                                    'flex items-center gap-2 p-2 cursor-pointer rounded-full text-sm text-zinc-900 dark:text-zinc-100',
-                                    active && 'bg-zinc-400 dark:bg-zinc-700'
-                                  )}
-                                >
-                                  {num || 'Unlimited'} Views
-                                </div>
-                              )}
-                            </Listbox.Option>
-                          ))}
-                        </div>
-                      </Listbox.Options>
-                    </>
-                  )}
-                </Listbox>
+                <textarea
+                  rows={5}
+                  maxLength={10000}
+                  value={secretData.text}
+                  className="w-full ph-no-capture"
+                  onChange={(e) => handleTextChange(e.target.value)}
+                ></textarea>
               </div>
-            </div>
 
-            <div className="text-sm text-neutral-500 flex items-center gap-2">
-              <FaInfo /> {boxExpiryString(expiry.getExpiry() || undefined, allowedViews)}
-            </div>
+              <div className="grid grid-cols-3 gap-8">
+                <div className="col-span-2">
+                  <RadioGroup value={expiry} by={compareExpiryOptions} onChange={setExpiry}>
+                    <RadioGroup.Label as={Fragment}>
+                      <label className="block text-gray-700 text-sm font-bold mb-2">Expiry</label>
+                    </RadioGroup.Label>
+                    <div className="flex flex-wrap items-center gap-3">
+                      {lockboxExpiryOptions.map((option) => (
+                        <RadioGroup.Option key={option.name} value={option} as={Fragment}>
+                          {({ active, checked }) => (
+                            <div
+                              className={clsx(
+                                'flex items-center gap-2 py-0.5 px-2 cursor-pointer bg-zinc-800 border border-zinc-800  rounded-full text-sm',
+                                active && 'border-zinc-700',
+                                checked && 'bg-zinc-700'
+                              )}
+                            >
+                              {checked ? (
+                                <FaDotCircle className="text-emerald-500" />
+                              ) : (
+                                <FaCircle />
+                              )}
+                              {option.name}
+                            </div>
+                          )}
+                        </RadioGroup.Option>
+                      ))}
+                    </div>
+                  </RadioGroup>
+                </div>
 
-            <div className="flex justify-end">
-              <Button type="submit" variant="primary" isLoading={loading}>
-                <FaShareAlt /> Generate link
-              </Button>
-            </div>
-          </form>
+                <div className="relative">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="secret">
+                    Maximum allowed views
+                  </label>
+                  <Listbox value={allowedViews} onChange={setAllowedViews}>
+                    {({ open }) => (
+                      <>
+                        <Listbox.Button as={Fragment} aria-required>
+                          <div className="p-2 flex items-center justify-between  rounded-md h-10 cursor-pointer bg-zinc-200 dark:bg-zinc-800 text-zinc-900 dark:text-zinc-100">
+                            {allowedViews || 'Unlimited'}
+                            <FaChevronDown
+                              className={clsx(
+                                'transition-transform ease duration-300 text-neutral-500',
+                                open ? 'rotate-180' : 'rotate-0'
+                              )}
+                            />
+                          </div>
+                        </Listbox.Button>
+                        <Listbox.Options>
+                          <div className="bg-zinc-300 dark:bg-zinc-800 p-2 rounded-md shadow-2xl absolute mt-1 z-20 ring-1 ring-inset ring-neutral-500/40 w-full">
+                            {lockboxAllowedViews.map((num: number | undefined) => (
+                              <Listbox.Option key={num} value={num} as={Fragment}>
+                                {({ active, selected }) => (
+                                  <div
+                                    className={clsx(
+                                      'flex items-center gap-2 p-2 cursor-pointer rounded-full text-sm text-zinc-900 dark:text-zinc-100',
+                                      active && 'bg-zinc-400 dark:bg-zinc-700'
+                                    )}
+                                  >
+                                    {num || 'Unlimited'} Views
+                                  </div>
+                                )}
+                              </Listbox.Option>
+                            ))}
+                          </div>
+                        </Listbox.Options>
+                      </>
+                    )}
+                  </Listbox>
+                </div>
+              </div>
+
+              <div className="text-sm text-neutral-500 flex items-center gap-2">
+                <FaInfo /> {boxExpiryString(expiry.getExpiry() || undefined, allowedViews)}
+              </div>
+
+              <div className="flex justify-end">
+                <Button type="submit" variant="primary" isLoading={loading}>
+                  <FaShareAlt /> Generate link
+                </Button>
+              </div>
+            </form>
+          </div>
         </div>
       )}
     </GenericDialog>
