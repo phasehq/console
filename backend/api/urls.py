43ee6bab6bdba50858d9e00f41e5174752e7946b
rohan-chaturvedi <rohan.chaturvedi@protonmail.com>
2024-05-01 13:46:21 +0530

    chore: organize views into separate files

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from .views import GoogleLoginView, GitHubLoginView, GitLabLoginView
+from .views.auth import GoogleLoginView, GitHubLoginView, GitLabLoginView
 
 urlpatterns = [
-    path('google/', GoogleLoginView.as_view(), name='google'),
-    path('github/', GitHubLoginView.as_view(), name='github'),
-    path('gitlab/', GitLabLoginView.as_view(), name='gitlab')
+    path("google/", GoogleLoginView.as_view(), name="google"),
+    path("github/", GitHubLoginView.as_view(), name="github"),
+    path("gitlab/", GitLabLoginView.as_view(), name="gitlab"),
 ]
