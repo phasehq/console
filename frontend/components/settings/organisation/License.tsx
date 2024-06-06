4f3bffdd14d5ce3851b94c5bd5bfb3d2af5b977b
rohan-chaturvedi <rohan.chaturvedi@protonmail.com>
2024-06-06 17:10:08 +0530

    refactor: rework license component

--- frontend/components/settings/organisation/License.tsx
+++ frontend/components/settings/organisation/License.tsx
@@ -1,14 +1,13 @@
-import {
-  ActivatedPhaseLicenseType,
-  ApiOrganisationPlanChoices,
-  PhaseLicenseType,
-  PlanTier,
-} from '@/apollo/graphql'
+import { ActivatedPhaseLicenseType, PhaseLicenseType, PlanTier } from '@/apollo/graphql'
 import { relativeTimeFromDates } from '@/utils/time'
+import clsx from 'clsx'
 import { MdVerified } from 'react-icons/md'
 
-export const License = (props: { license: PhaseLicenseType | ActivatedPhaseLicenseType }) => {
-  const { license } = props
+export const License = (props: {
+  license: PhaseLicenseType | ActivatedPhaseLicenseType
+  showExpiry: boolean
+}) => {
+  const { license, showExpiry } = props
 
   const planString = () => {
     if (license.plan === PlanTier.EnterprisePlan || license.plan === 'EN') return 'Enterprise'
@@ -16,13 +15,22 @@ export const License = (props: { license: PhaseLicenseType | ActivatedPhaseLicen
     else return license.plan
   }
 
+  const isExpired = () => new Date() > new Date(license.expiresAt)
+
   return (
-    <div className="ring-1 ring-inset ring-emerald-400/20 bg-emerald-400/10 rounded-lg p-4 w-full flex items-center justify-between gap-4">
-      <MdVerified className="text-emerald-400 text-2xl shrink-0" />
+    <div
+      className={clsx(
+        'ring-1 ring-inset  rounded-lg p-4 w-full flex items-center justify-between gap-4',
+        isExpired()
+          ? 'ring-red-400/20 bg-red-400/20 dark:bg-red-400/10'
+          : 'ring-emerald-400/20 bg-emerald-400/20 dark:bg-emerald-400/10'
+      )}
+    >
+      <MdVerified
+        className={clsx('text-2xl shrink-0', isExpired() ? 'text-red-400' : 'text-emerald-400')}
+      />
       <div className="w-full">
-        <div className="flex items-center gap-2 text-lg font-semibold">
-          Phase {planString()} License
-        </div>
+        <div className="flex items-center gap-2 text-lg font-semibold">Phase {planString()}</div>
         <div className="font-mono text-neutral-500 text-sm">{license.id}</div>
       </div>
       <div className="text-neutral-500 w-full flex flex-col items-end">
@@ -32,7 +40,12 @@ export const License = (props: { license: PhaseLicenseType | ActivatedPhaseLicen
             {license.customerName}
           </span>
         </div>
-        <div className="text-xs">Expires {relativeTimeFromDates(new Date(license.expiresAt))}</div>
+        {showExpiry && (
+          <div className={clsx('text-xs', isExpired() && 'text-red-400')}>
+            {isExpired() ? 'Expired' : 'Valid till'}{' '}
+            {relativeTimeFromDates(new Date(license.expiresAt))}
+          </div>
+        )}
       </div>
     </div>
   )
