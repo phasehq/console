dd9498b4f2c5d51420f7bdd2d84bea8cf2260cdc
rohan-chaturvedi <rohan.chaturvedi@protonmail.com>
2024-08-04 15:16:44 +0530

    feat: restyle checkout preview screen

--- frontend/ee/billing/ProUpgradeDialog.tsx
+++ frontend/ee/billing/ProUpgradeDialog.tsx
@@ -12,8 +12,11 @@ import { ApiOrganisationPlanChoices } from '@/apollo/graphql'
 import { FaArrowRight, FaCheckCircle, FaUser } from 'react-icons/fa'
 import { Button } from '@/components/common/Button'
 import { FaCartShopping } from 'react-icons/fa6'
+import { ToggleSwitch } from '@/components/common/ToggleSwitch'
 
 type BillingPeriods = 'monthly' | 'yearly'
+
+type PriceOption = { name: BillingPeriods; unitPrice: number; monthlyPrice: number }
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!)
 
@@ -44,7 +47,7 @@ const UpgradeForm = (props: { onSuccess: Function; billingPeriod: BillingPeriods
   )
 }
 
-const prices: { name: BillingPeriods; unitPrice: number; monthlyPrice: number }[] = [
+const prices: PriceOption[] = [
   {
     name: 'monthly',
     unitPrice: 18,
@@ -58,7 +61,59 @@ const prices: { name: BillingPeriods; unitPrice: number; monthlyPrice: number }[
 ]
 
 export const ProUpgradeDialog = (props: { userCount: number }) => {
+  const [checkoutPreview, setCheckoutPreview] = useState<BillingPeriods>('yearly')
   const [billingPeriod, setBillingPeriod] = useState<BillingPeriods | null>(null)
+
+  const toggleCheckoutPreview = () => {
+    if (checkoutPreview === 'yearly') setCheckoutPreview('monthly')
+    else setCheckoutPreview('yearly')
+  }
+
+  const priceToPreview = prices.find((price) => price.name === checkoutPreview)
+
+  const CheckoutPreview = ({ price }: { price: PriceOption }) => (
+    <div
+      key={price.name}
+      className="group shadow-xl bg-zinc-100 dark:bg-zinc-800 ring-1 ring-inset ring-neutral-500/40 rounded-lg p-4 space-y-6 transition ease"
+    >
+      <div className="flex items-start justify-between">
+        <div className="text-zinc-900 dark:text-zinc-100">
+          <span className="font-extralight text-7xl">${price.monthlyPrice}</span>
+          <span className="text-neutral-500">/mo per user</span>
+        </div>
+        <div>
+          <div className="text-neutral-500 text-xs uppercase font-medium">Billed</div>
+          <div className="flex items-center justify-center gap-2 text-zinc-900 dark:text-zinc-100 text-xs">
+            <div>Monthly</div>
+            <ToggleSwitch value={checkoutPreview === 'yearly'} onToggle={toggleCheckoutPreview} />
+            <div>Yearly</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-zinc-200 dark:bg-zinc-700 p-3 rounded-lg mt-4 text-zinc-900 dark:text-zinc-100 text-xs space-y-1">
+        <div className="flex justify-between text-zinc-900 dark:text-zinc-100">
+          <span>Unit Price:</span>
+          <span>${price.unitPrice}</span>
+        </div>
+        <div className="flex justify-between text-zinc-900 dark:text-zinc-100">
+          <span>Number of Users:</span>
+          <span>{props.userCount}</span>
+        </div>
+        <hr className="my-2 border-zinc-300 dark:border-zinc-600" />
+        <div className="flex justify-between font-semibold text-zinc-900 dark:text-zinc-100">
+          <span>Total:</span>
+          <span>${price.unitPrice * props.userCount}</span>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-1 text-emerald-500 font-medium pt-4 justify-end">
+        <Button variant="primary" onClick={() => setBillingPeriod(price.name)}>
+          <FaCartShopping /> Checkout
+        </Button>
+      </div>
+    </div>
+  )
 
   if (billingPeriod === null)
     return (
@@ -68,44 +123,7 @@ export const ProUpgradeDialog = (props: { userCount: number }) => {
           <PlanLabel plan={ApiOrganisationPlanChoices.Pr} />
         </div>
 
-        <div className="grid grid-cols-2 gap-8">
-          {prices.map((price) => (
-            <div
-              key={price.name}
-              className="group shadow-xl bg-zinc-100 dark:bg-zinc-800 ring-1 ring-inset ring-neutral-500/40 rounded-lg p-4 space-y-4 transition ease"
-            >
-              <div className="text-zinc-900 dark:text-zinc-100 font-semibold text-lg capitalize">
-                Pay {price.name}
-              </div>
-              <div className="text-zinc-900 dark:text-zinc-100">
-                <span className="font-extralight text-7xl">${price.monthlyPrice}</span>
-                <span className="text-neutral-500">/mo per user</span>
-              </div>
-
-              <div className="bg-zinc-200 dark:bg-zinc-700 p-3 rounded-lg mt-4 text-zinc-900 dark:text-zinc-100 text-xs space-y-1">
-                <div className="flex justify-between text-zinc-900 dark:text-zinc-100">
-                  <span>Unit Price:</span>
-                  <span>${price.unitPrice}</span>
-                </div>
-                <div className="flex justify-between text-zinc-900 dark:text-zinc-100">
-                  <span>Number of Users:</span>
-                  <span>{props.userCount}</span>
-                </div>
-                <hr className="my-2 border-zinc-300 dark:border-zinc-600" />
-                <div className="flex justify-between font-semibold text-zinc-900 dark:text-zinc-100">
-                  <span>Total:</span>
-                  <span>${price.unitPrice * props.userCount}</span>
-                </div>
-              </div>
-
-              <div className="flex items-center gap-1 text-emerald-500 font-medium pt-4 justify-end">
-                <Button variant="primary" onClick={() => setBillingPeriod(price.name)}>
-                  <FaCartShopping /> Checkout
-                </Button>
-              </div>
-            </div>
-          ))}
-        </div>
+        <div>{priceToPreview && <CheckoutPreview price={priceToPreview} />}</div>
       </div>
     )
   return (
