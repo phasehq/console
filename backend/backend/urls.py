a8e80248d33c93cba11de0995bd37a0538e4e362
Nimish <85357445+nimish-ks@users.noreply.github.com>
2024-08-07 11:57:48 +0530

    fix: removed uuid from health endpoint (#329)
    * fix: removed uuid from health endpoint
    
    * fix: support legacy health endpoint

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,8 @@ urlpatterns = [
     path("social/login/", include("api.urls")),
     path("logout/", csrf_exempt(logout_view)),
     path("graphql/", csrf_exempt(PrivateGraphQLView.as_view(graphiql=True))),
+    path("/health", health_check),
+    # To not break legacy health checks. TODO: Remove
     path("493c5048-99f9-4eac-ad0d-98c3740b491f/health", health_check),
     path("secrets/", E2EESecretsView.as_view()),
     path("public/v1/secrets/", PublicSecretsView.as_view()),
