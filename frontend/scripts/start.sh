bae2759e22bc5b3f4c38fbcc833836d4dc9ce144
Nimish <nimish@phase.dev>
2024-08-07 15:10:46 +0530

    fix: added hostname

--- frontend/scripts/start.sh
+++ frontend/scripts/start.sh
@@ -2,4 +2,4 @@
 
 # Set up runtime env vars and start next server
 bash scripts/replace-variable.sh && 
-NODE_ENV=production HOST=0.0.0.0 PORT=3000 node server.js
+NODE_ENV=production HOST=0.0.0.0 HOSTNAME=0.0.0.0 PORT=3000 node server.js
