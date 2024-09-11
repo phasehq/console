22c103a86e5cb2d7584d512b1dd3555002b7fa11
Rohan <rohan.chaturvedi@protonmail.com>
2024-09-11 21:17:52 +0530

    refactor: simplify policy structure

--- backend/api/utils/access/roles.py
+++ backend/api/utils/access/roles.py
@@ -1,110 +1,59 @@
 default_roles = {
     "Owner": {
-        "permissions": [
-            {
-                "resource": "Organisation",
-                "actions": ["create", "read", "update", "delete"],
-            },
-            {"resource": "Billing", "actions": ["create", "read", "update", "delete"]},
-            {"resource": "Apps", "actions": ["create", "read", "update", "delete"]},
-            {"resource": "Members", "actions": ["create", "read", "update", "delete"]},
-            {
-                "resource": "ServiceAccounts",
-                "actions": ["create", "read", "update", "delete"],
-            },
-            {"resource": "Roles", "actions": ["create", "read", "update", "delete"]},
-            {
-                "app_permissions": [
-                    {
-                        "resource": "Environments",
-                        "actions": ["create", "read", "update", "delete"],
-                    },
-                    {
-                        "resource": "Secrets",
-                        "actions": ["create", "read", "update", "delete"],
-                    },
-                    {
-                        "resource": "Logs",
-                        "actions": ["create", "read", "update", "delete"],
-                    },
-                    {
-                        "resource": "Tokens",
-                        "actions": ["create", "read", "update", "delete"],
-                    },
-                    {
-                        "resource": "Members",
-                        "actions": ["create", "read", "update", "delete"],
-                    },
-                ]
-            },
-        ]
+        "permissions": {
+            "Organisation": ["create", "read", "update", "delete"],
+            "Billing": ["create", "read", "update", "delete"],
+            "Apps": ["create", "read", "update", "delete"],
+            "Members": ["create", "read", "update", "delete"],
+            "ServiceAccounts": ["create", "read", "update", "delete"],
+            "Roles": ["create", "read", "update", "delete"],
+        },
+        "app_permissions": {
+            "Environments": ["create", "read", "update", "delete"],
+            "Secrets": ["create", "read", "update", "delete"],
+            "Logs": ["create", "read", "update", "delete"],
+            "Tokens": ["create", "read", "update", "delete"],
+            "Members": ["create", "read", "update", "delete"],
+            "Integrations": ["create", "read", "update", "delete"],
+            "EncryptionMode": ["read", "update"],
+        },
     },
     "Admin": {
-        "permissions": [
-            {
-                "resource": "Organisation",
-                "actions": [
-                    "read",
-                    "update",
-                ],
-            },
-            {"resource": "Billing", "actions": ["create", "read", "update", "delete"]},
-            {"resource": "Apps", "actions": ["create", "read", "update", "delete"]},
-            {"resource": "Members", "actions": ["create", "read", "update", "delete"]},
-            {
-                "resource": "ServiceAccounts",
-                "actions": ["create", "read", "update", "delete"],
-            },
-            {"resource": "Roles", "actions": ["create", "read", "update", "delete"]},
-            {
-                "app_permissions": [
-                    {
-                        "resource": "Environments",
-                        "actions": ["create", "read", "update", "delete"],
-                    },
-                    {
-                        "resource": "Secrets",
-                        "actions": ["create", "read", "update", "delete"],
-                    },
-                    {
-                        "resource": "Logs",
-                        "actions": ["create", "read", "update", "delete"],
-                    },
-                    {
-                        "resource": "Tokens",
-                        "actions": ["create", "read", "update", "delete"],
-                    },
-                    {
-                        "resource": "Members",
-                        "actions": ["create", "read", "update", "delete"],
-                    },
-                ]
-            },
-        ]
+        "permissions": {
+            "Organisation": ["read", "update"],
+            "Billing": ["create", "read", "update", "delete"],
+            "Apps": ["create", "read", "update", "delete"],
+            "Members": ["create", "read", "update", "delete"],
+            "ServiceAccounts": ["create", "read", "update", "delete"],
+            "Roles": ["create", "read", "update", "delete"],
+        },
+        "app_permissions": {
+            "Environments": ["create", "read", "update", "delete"],
+            "Secrets": ["create", "read", "update", "delete"],
+            "Logs": ["create", "read", "update", "delete"],
+            "Tokens": ["create", "read", "update", "delete"],
+            "Members": ["create", "read", "update", "delete"],
+            "Integrations": ["create", "read", "update", "delete"],
+            "EncryptionMode": ["read", "update"],
+        },
     },
     "Developer": {
-        "permissions": [
-            {"resource": "Organisation", "actions": []},
-            {"resource": "Billing", "actions": []},
-            {"resource": "Apps", "actions": ["read"]},
-            {"resource": "Members", "actions": ["read"]},
-            {"resource": "ServiceAccounts", "actions": []},
-            {"resource": "Roles", "actions": []},
-            {
-                "app_permissions": [
-                    {
-                        "resource": "Environments",
-                        "actions": ["read", "create", "update"],
-                    },
-                    {
-                        "resource": "Secrets",
-                        "actions": ["create", "read", "update", "delete"],
-                    },
-                    {"resource": "Logs", "actions": ["read"]},
-                    {"resource": "Tokens", "actions": ["read", "create"]},
-                    {"resource": "Members", "actions": ["read"]},
-                ]
-            },
-        ]
+        "permissions": {
+            "Organisation": [],
+            "Billing": [],
+            "Apps": ["read"],
+            "Members": ["read"],
+            "ServiceAccounts": [],
+            "Roles": [],
+        },
+        "app_permissions": {
+            "Environments": ["read", "create", "update"],
+            "Secrets": ["create", "read", "update", "delete"],
+            "Logs": ["read"],
+            "Tokens": ["read", "create"],
+            "Members": ["read"],
+            "Integrations": ["create", "read", "update", "delete"],
+            "EncryptionMode": ["read", "update"],
+        },
     },
 }
