0d0b3c4530667bad330d5c9c65011449f4a1499f
Nimish <nimish@phase.dev>
2024-09-19 12:18:57 +0530

    fix: conver deploy changes button into a div

--- frontend/components/environments/secrets/DeployStatusBar.tsx
+++ frontend/components/environments/secrets/DeployStatusBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from '@/components/common/Button';
 import { FaCheckCircle } from 'react-icons/fa';
+import { IoCloudUploadSharp } from "react-icons/io5";
 import { DeployPreview } from '@/components/environments/secrets/DeployPreview';
 import clsx from 'clsx';
 
@@ -34,24 +35,27 @@ const DeployStatusBar: React.FC<DeployStatusBarProps> = ({
             onDiscard={onDiscard}
             isLoading={isLoading}
           />
-          <Button
-            variant="warning"
+          <div
             onClick={onDeploy}
-            disabled={isLoading}
-            isLoading={isLoading}
             className={clsx(
-              "whitespace-nowrap",
-              "transition-all duration-300 ease-in-out"
+              "flex items-center gap-2",
+              "rounded-full py-2 px-4",
+              "text-amber-500 text-base",
+              "ring-1 ring-inset ring-amber-500/20",
+              "transition-all duration-300 ease-in-out",
+              "cursor-pointer",
+              { "opacity-50 cursor-not-allowed": isLoading }
             )}
           >
-            {isLoading ? 'Deploying...' : 'Deploy changes'}
-          </Button>
+            <IoCloudUploadSharp className="shrink-0" />
+            <span>{isLoading ? 'Deploying...' : 'Deploy changes'}</span>
+          </div>
         </>
       ) : (
         <div className={clsx(
           "flex items-center gap-2",
-          "rounded-full py-1 px-3",
-          "text-emerald-500",
+          "rounded-full py-2 px-4",
+          "text-emerald-500 text-base",
           "ring-1 ring-inset ring-emerald-500/20",
           "transition-all duration-300 ease-in-out",
         )}>
