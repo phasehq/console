250f9f32727fb5ee65d29f5643a1c89ec7b98388
Rohan <rohan.chaturvedi@protonmail.com>
2024-09-23 14:19:48 +0530

    fix: get default role description from meta field

--- backend/backend/graphene/types.py
+++ backend/backend/graphene/types.py
@@ -47,6 +47,7 @@ class OrganisationPlanType(ObjectType):
 class RoleType(DjangoObjectType):
     name = graphene.String()
     description = graphene.String()
+    color = graphene.String()
     permissions = graphene.JSONString()
     is_default = graphene.Boolean()
 
@@ -58,6 +59,11 @@ class RoleType(DjangoObjectType):
         if self.is_default:
             return default_roles.get(self.name, {})
         return self.permissions
+
+    def resolve_description(self, info):
+        if self.is_default:
+            return default_roles.get(self.name, {})["meta"]["description"]
+        return self.description
 
 
 class OrganisationType(DjangoObjectType):
