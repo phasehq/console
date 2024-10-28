d10d6b32bc745e384942ce5e3ac5a359dae3cd4b
Nimish <nimish@phase.dev>
2024-10-28 15:36:27 +0530

    wip: don't capture url fragments

--- frontend/utils/posthog.ts
+++ frontend/utils/posthog.ts
@@ -9,6 +9,23 @@ export function initializePostHog() {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
       capture_pageview: true,
+      // Sanitize properties to remove URL fragments
+      sanitize_properties: (properties, event_name) => {
+        const sanitized = { ...properties }
+        
+        // Clean URL fragments from $current_url
+        if (sanitized.$current_url) {
+          sanitized.$current_url = sanitized.$current_url.split('#')[0]
+        }
+        
+        // Clean URL fragments from $referrer
+        if (sanitized.$referrer) {
+          sanitized.$referrer = sanitized.$referrer.split('#')[0]
+        }
+
+        return sanitized
+      },
+      // Additional security for session recordings
       session_recording: {
         maskInputOptions: {
           password: true, // Mask password inputs
