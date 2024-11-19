cdb3ce0e31fad78dbea6ed45cd39fac06d53c8ba
Rohan <rohan.chaturvedi@protonmail.com>
2024-11-19 20:04:09 +0530

    fix: permission checks and rendering for onboarding guide

--- frontend/components/dashboard/GetStarted.tsx
+++ frontend/components/dashboard/GetStarted.tsx
@@ -124,7 +124,8 @@ export const GetStarted = (props: { organisation: OrganisationType }) => {
   const userCanReadSyncs = userHasPermission(
     organisation?.role?.permissions,
     'Integrations',
-    'read'
+    'read',
+    true
   )
 
   const hasPermissionsForGuide =
@@ -228,6 +229,8 @@ export const GetStarted = (props: { organisation: OrganisationType }) => {
       linkText: 'Join',
     },
   ]
+
+  if (!hasPermissionsForGuide) return <></>
 
   if (!showGuide)
     return (
