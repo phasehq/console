ea9e43310fa6460eaa75223d3e85422e01a3a361
Nimish <nimish@phase.dev>
2025-01-04 16:25:50 +0530

    feat: improved start script migrations verbosity for better clarity

--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 set -e
 
-# If set to true the Django database migrations needs be run externally 
-if [ "$EXTERNAL_MIGRATION" != "true" ]; then
-    echo "Running migrations..."
+echo "Checking migration configuration..."
+if [ "$EXTERNAL_MIGRATION" = "true" ]; then
+    echo "EXTERNAL_MIGRATION flag is set to true. Skipping migrations as they will be handled externally."
+else
+    echo "EXTERNAL_MIGRATION flag is not set. Running migrations locally..."
     python manage.py migrate
 fi
 
-echo "Starting gunicorn..."
+echo "Starting gunicorn server..."
 exec gunicorn -b '[::]:8000' --workers 3 backend.wsgi:application
