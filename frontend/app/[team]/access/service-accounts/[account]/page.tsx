ce3dcff9a07cb4b52e60c6e8011bac5047e69b6a
Nimish <nimish@phase.dev>
2025-01-31 15:34:04 +0530

    fix: improved column alignment

--- frontend/app/[team]/access/service-accounts/[account]/page.tsx
+++ frontend/app/[team]/access/service-accounts/[account]/page.tsx
@@ -300,93 +300,90 @@ export default function ServiceAccount({ params }: { params: { team: string; acc
             <div className="space-y-2 divide-y divide-neutral-500/20 py-4">
               {account.tokens && account.tokens.length > 0 ? (
                 account.tokens.map((token) => (
-                <div key={token!.id} className="grid grid-cols-1 md:grid-cols-5 gap-4 items-center p-2 group">
-                  {/* Token Name and ID*/}
-                  <div className="space-y-1">
-                    <div className="flex items-center gap-2">
-                      <FaKey className="text-neutral-500 flex-shrink-0" />
-                      <span className="font-medium text-lg text-zinc-900 dark:text-zinc-100 truncate">
-                        {token!.name}
-                      </span>
-                    </div>
-                    <div className="flex items-center gap-2 text-sm text-neutral-500 group/id">
-                      <span className="font-mono">{token!.id}</span>
-                      <span className="opacity-0 group-hover/id:opacity-100 transition ease">
-                        <CopyButton value={token!.id || ''} defaultHidden />
-                      </span>
-                    </div>
-                  </div>
-
-                  {/* Created Info */}
-                  <div className="col-span-2 text-neutral-500 text-sm flex flex-wrap items-center gap-1">
-                    <div className="flex items-center gap-1 min-w-0">
-                      <span className="whitespace-nowrap">Created</span>
-                      <span className="whitespace-nowrap">{relativeTimeFromDates(new Date(token?.createdAt))}</span>
-                      <span className="whitespace-nowrap">by</span>
-                    </div>
-                    <div className="flex items-center gap-1 flex-shrink-0">
-                      <Avatar imagePath={token!.createdBy?.avatarUrl} size="sm" />
-                      <span className="font-medium text-zinc-900 dark:text-zinc-100">
-                        {token?.createdBy?.fullName}
-                      </span>
-                    </div>
-                  </div>
-
-                  {/* Token Status */}
-                  <div className="space-y-2">
-                    {/* Expires */}
-                    <div className="flex items-center gap-1 text-sm text-neutral-500">
-                      <span className="whitespace-nowrap">Expires:</span>
-                      <span className="whitespace-nowrap">
-                        {token!.expiresAt ? relativeTimeFromDates(new Date(token?.expiresAt)) : 'never'}
-                      </span>
+                  <div key={token!.id} className="grid grid-cols-1 md:grid-cols-12 gap-4 items-center p-2 group">
+                    {/* Token Name and ID*/}
+                    <div className="md:col-span-4 space-y-1">
+                      <div className="flex items-center gap-2">
+                        <FaKey className="text-neutral-500 flex-shrink-0" />
+                        <span className="font-medium text-lg text-zinc-900 dark:text-zinc-100 truncate">
+                          {token!.name}
+                        </span>
+                      </div>
+                      <div className="flex items-center gap-2 text-sm text-neutral-500 group/id">
+                        <span className="font-mono">{token!.id}</span>
+                        <span className="opacity-0 group-hover/id:opacity-100 transition ease">
+                          <CopyButton value={token!.id || ''} defaultHidden />
+                        </span>
+                      </div>
                     </div>
 
-                    {/* Last Used */}
-                    <div className="flex items-center gap-1 text-sm text-neutral-500">
-                      <span className="whitespace-nowrap">Last used:</span>
-                      <span className="whitespace-nowrap">
-                        {token!.lastUsed ? relativeTimeFromDates(new Date(token?.lastUsed)) : 'never'}
-                      </span>
+                    {/* Created Info*/}
+                    <div className="md:col-span-4 text-neutral-500 text-sm flex flex-col gap-1">
+                      <div className="whitespace-nowrap">
+                        Created {relativeTimeFromDates(new Date(token?.createdAt))}
+                      </div>
+                      <div className="flex items-center gap-2">
+                        <span className="text-neutral-500">by:</span>
+                        <Avatar imagePath={token!.createdBy?.avatarUrl} size="sm" />
+                        <span className="font-medium text-zinc-900 dark:text-zinc-100">
+                          {token?.createdBy?.fullName}
+                        </span>
+                      </div>
+                    </div>
+
+                    {/* Token Status*/}
+                    <div className="md:col-span-3 space-y-2">
+                      <div className="flex items-center gap-1 text-sm text-neutral-500">
+                        <span className="whitespace-nowrap">Expires:</span>
+                        <span className="whitespace-nowrap">
+                          {token!.expiresAt ? relativeTimeFromDates(new Date(token?.expiresAt)) : 'never'}
+                        </span>
+                      </div>
+
+                      <div className="flex items-center gap-1 text-sm text-neutral-500">
+                        <span className="whitespace-nowrap">Last used:</span>
+                        <span className="whitespace-nowrap">
+                          {token!.lastUsed ? relativeTimeFromDates(new Date(token?.lastUsed)) : 'never'}
+                        </span>
+                      </div>
+                    </div>
+
+                    {/* Delete Button*/}
+                    <div className="md:col-span-1 flex justify-end opacity-0 group-hover:opacity-100 transition ease">
+                      <DeleteServiceAccountTokenDialog token={token!} />
                     </div>
                   </div>
-
-                  {/* Delete Button */}
-                  <div className="flex justify-end opacity-0 group-hover:opacity-100 transition ease">
-                    <DeleteServiceAccountTokenDialog token={token!} />
-                  </div>
+                ))
+              ) : (
+                <div className="py-8">
+                  <EmptyState
+                    title="No tokens created"
+                    subtitle="This Service Account does not have any tokens. Create a new token to access secrets from Apps associated with this account."
+                    graphic={
+                      <div className="text-neutral-300 dark:text-neutral-700 text-7xl text-center">
+                        <FaKey />
+                      </div>
+                    }
+                  >
+                    <></>
+                  </EmptyState>
                 </div>
-              ))
-            ) : (
-              <div className="py-8">
-                <EmptyState
-                  title="No tokens created"
-                  subtitle="This Service Account does not have any tokens. Create a new token to access secrets from Apps associated with this account."
-                  graphic={
-                    <div className="text-neutral-300 dark:text-neutral-700 text-7xl text-center">
-                      <FaKey />
-                    </div>
-                  }
-                >
-                  <></>
-                </EmptyState>
-              </div>
-            )}
-          </div>
-        ) : (
-          <div className="py-8">
-            <EmptyState
-              title="Access restricted"
-              subtitle="You don't have the permissions required to view Service Account Tokens"
-              graphic={
-                <div className="text-neutral-300 dark:text-neutral-700 text-7xl text-center">
-                  <FaBan />
-                </div>
-              }
-            >
-              <></>
-            </EmptyState>
-          </div>
+              )}
+            </div>
+          ) : (
+            <div className="py-8">
+              <EmptyState
+                title="Access restricted"
+                subtitle="You don't have the permissions required to view Service Account Tokens"
+                graphic={
+                  <div className="text-neutral-300 dark:text-neutral-700 text-7xl text-center">
+                    <FaBan />
+                  </div>
+                }
+              >
+                <></>
+              </EmptyState>
+            </div>
           )}
         </div>
 
