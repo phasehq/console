27d940efd1054832f5ddd7c568cc9ea6725f2f42
Rohan Chaturvedi <rohan.chaturvedi@protonmail.com>
2025-03-24 20:34:57 +0530

    refactor: apps list page (#486)
    * feat: rework dialog trigger
    
    * fix: add more fasly check for enc mode indicator
    
    * refactor: rework apps list page with list and grid view modes, empty states, etc
    
    * refactor: rework app card
    
    * fix: typo
    
    * fix: add variant prop to app card in integrations page
    
    * feat: overlap stack element icons, misc fixes
    
    * feat: rework list view
    
    * fix: show title even in label mode
    
    * fix: prevent button label wrap
    
    * fix: tweak auto-collapse width to use standard lg breakpoint
    
    * feat: add size variant prop to provider icon
    
    * fix: misc fixes
    
    * fix: revert provider icon
    
    * fix: misc fixes
    
    * fix: update dark background color for AppCard component
    
    * feat: add sort menu, misc fixes
    
    * feat: add last updated col, improve mobile view and responsiveness
    
    * feat: add optional showTitle prop to avatar
    
    * feat: misc layout fixes
    
    * fix: misc fixes to layout for each variant
    
    * feat: misc improvements to syncs ux for large number of apps
    
    * fix: table header alignment
    
    * fix: typo
    
    * feat: update add app menu for service accounts
    
    * feat: add loading skeletons, misc fixes to empty states
    
    * fix: add loading prop to apps view
    
    * feat: useMemo to optimize search and sort
    
    * fix: don't show skeletons after initial load
    
    * fix: fetching environments
    
    * feat: use memo for providers
    
    * feat: changed third party integration tab titles, subtitles
    
    * fix: column alignment
    
    * fix: update syncs description for clarity
    
    * fix: prevent event propagation on CopyButton click in AppCard (#511)
    
    * fix: set default tab index for integrations based on URL (#512)
    
    ---------
    
    Co-authored-by: Nimish <85357445+nimish-ks@users.noreply.github.com>
    Co-authored-by: Nimish <nimish@phase.dev>

--- frontend/components/apps/AppCard.tsx
+++ frontend/components/apps/AppCard.tsx
@@ -1,18 +1,37 @@
 import { FaProjectDiagram, FaRobot, FaUsers } from 'react-icons/fa'
 import { Card } from '../common/Card'
-
 import { AppType } from '@/apollo/graphql'
 import { ProviderIcon } from '../syncing/ProviderIcon'
 import { Avatar } from '../common/Avatar'
 import { BsListColumnsReverse } from 'react-icons/bs'
 import { EncryptionModeIndicator } from './EncryptionModeIndicator'
+import clsx from 'clsx'
+import { ReactNode, useContext } from 'react'
+import CopyButton from '../common/CopyButton'
+import { organisationContext } from '@/contexts/organisationContext'
+import Link from 'next/link'
+import { relativeTimeFromDates } from '@/utils/time'
+import { useRouter } from 'next/navigation'
+import { AppTabs } from '@/utils/app'
 
 interface AppCardProps {
   app: AppType
+  variant: 'normal' | 'compact'
+  tabToLink?: AppTabs
 }
 
-export const AppCard = (props: AppCardProps) => {
-  const { name, id, members, serviceAccounts, environments } = props.app
+interface AppCardMetaProps {
+  icon: React.ReactElement
+  count: number
+  itemType: string
+  children: ReactNode
+  link?: string
+}
+
+const AppCardContent = ({ app, variant }: AppCardProps) => {
+  const { name, id, members, serviceAccounts, environments } = app
+
+  const { activeOrganisation: organisation } = useContext(organisationContext)
 
   const totalSyncCount = environments
     ? environments.reduce((acc, env) => acc + (env!.syncs?.length || 0), 0)
@@ -38,106 +57,316 @@ export const AppCard = (props: AppCardProps) => {
 
   const surplusSynCount = providers.length > 5 ? providers.length - 5 : 0
 
-  return (
-    <Card>
-      <div className="rounded-xl p-4 flex flex-col w-full gap-10 justify-between">
-        <div className="space-y-1">
-          <div className="text-2xl font-semibold flex items-center gap-2 text-zinc-700 dark:text-zinc-300 group-hover:text-zinc-900 dark:group-hover:text-zinc-100 transition ease">
-            {name} <EncryptionModeIndicator app={props.app} />
-          </div>
-          <div className="text-2xs font-mono text-neutral-500 w-full break-all text-left">{id}</div>
+  const AppMetaCategory: React.FC<AppCardMetaProps> = ({
+    icon,
+    count,
+    itemType,
+    children,
+    link,
+  }) => {
+    const content = () => (
+      <div className="space-y-1 hidden lg:block">
+        <div className={clsx(variant === 'normal' ? 'space-y-1' : 'flex items-center gap-4')}>
+          {variant === 'normal' && count > 0 && (
+            <div className="flex items-center gap-2 text-xl">
+              {icon}
+              <span className="font-light">{count}</span>
+            </div>
+          )}
+          {variant === 'normal' && count > 0 && (
+            <span className="text-neutral-500 font-medium text-[0.6rem] uppercase tracking-widest">
+              {count === 1 ? itemType : `${itemType}s`}
+            </span>
+          )}
+        </div>
+        <div className="lg:flex items-center gap-1 text-base hidden">{children}</div>
+      </div>
+    )
+
+    if (variant === 'normal') return count > 0 ? content() : <></>
+
+    if (variant === 'compact') {
+      if (count > 0) {
+        return link ? (
+          <Link
+            title={`View ${app.name} ${itemType}s`}
+            className="hidden lg:flex w-min group"
+            href={link}
+            onClick={(e) => e.stopPropagation()}
+          >
+            {content()}
+          </Link>
+        ) : (
+          content()
+        )
+      } else return <div></div>
+    }
+  }
+
+  const CondensedAppMetaCounts = () => {
+    return (
+      <div className="flex items-center gap-2">
+        <div className="flex items-center gap-1 text-xs text-neutral-500">
+          <FaUsers />
+          {members.length}
         </div>
 
-        <div className="flex items-center justify-between">
-          <div className="space-y-1">
-            <div className="flex items-center gap-2 text-2xl">
-              <FaUsers />
-              <span className="font-light">{members.length}</span>
-            </div>
-            <span className="text-neutral-500 font-medium text-2xs uppercase tracking-widest">
-              {members.length > 1 ? 'Members' : 'Member'}
-            </span>
-            <div className="flex items-center gap-1 text-base">
-              {members.slice(0, 5).map((member) => (
-                <Avatar key={member!.id} member={member || undefined} size="sm" />
-              ))}
-              {surplusMemberCount > 0 && (
-                <span className="text-neutral-500 text-xs">+{surplusMemberCount}</span>
-              )}
-            </div>
-          </div>
+        <div className="flex items-center gap-1 text-xs text-neutral-500">
+          <FaRobot />
+          {serviceAccounts.length}
+        </div>
 
-          {serviceAccounts.length > 0 && (
-            <div className="space-y-1">
-              <div className="flex items-center gap-2 text-2xl">
-                <FaRobot />
-                <span className="font-light">{serviceAccounts.length}</span>
-              </div>
-              <span className="text-neutral-500 font-medium text-2xs uppercase tracking-widest">
-                {serviceAccounts.length > 1 ? 'Service Accounts' : 'Service Account'}
-              </span>
-              <div className="flex items-center gap-1 text-base">
-                {serviceAccounts.slice(0, 5).map((account) => (
-                  <div
-                    key={account!.id}
-                    className="rounded-full flex items-center bg-neutral-500/40 justify-center size-5 p-1"
-                  >
-                    <span className="text-2xs font-semibold text-zinc-900 dark:text-zinc-100">
-                      {account?.name.slice(0, 1)}
-                    </span>
-                  </div>
-                ))}
-                {surplusMemberCount > 0 && (
-                  <span className="text-neutral-500 text-xs">+{surplusMemberCount}</span>
-                )}
-              </div>
-            </div>
-          )}
+        <div className="flex items-center gap-1 text-xs text-neutral-500">
+          <BsListColumnsReverse />
+          {environments.length}
+        </div>
 
-          <div className="space-y-1">
-            <div className="flex items-center gap-2 text-2xl">
-              <BsListColumnsReverse />
-              <span className="font-light">{environments.length}</span>
-            </div>
-            <span className="text-neutral-500 font-medium text-2xs uppercase tracking-widest">
-              {environments.length > 1 ? 'Environments' : 'Environment'}
-            </span>
-            <div className="flex items-center gap-1 text-base">
-              {environments.slice(0, 5).map((env) => (
-                <div
-                  key={env!.id}
-                  className="bg-neutral-400/10 ring-1 inset-inset ring-neutral-400/20 rounded-full px-2 text-zinc-800 dark:text-zinc-200 text-2xs font-semibold"
-                >
-                  {env!.name.slice(0, 1)}
-                </div>
-              ))}
-              {surplusEnvCount > 0 && (
-                <span className="text-neutral-500 text-xs">+{surplusEnvCount}</span>
-              )}
-            </div>
-          </div>
-
-          {totalSyncCount > 0 && (
-            <div className="space-y-1">
-              <div className="flex items-center gap-2 text-2xl">
-                <FaProjectDiagram />
-                <span className="font-light">{totalSyncCount}</span>
-              </div>
-              <span className="text-neutral-500 font-medium text-2xs uppercase tracking-widest">
-                {totalSyncCount > 1 ? 'Integrations' : 'Integration'}
-              </span>
-              <div className="flex items-center gap-2 text-base">
-                {providers.slice(0, 5).map((providerId) => (
-                  <ProviderIcon key={providerId} providerId={providerId} />
-                ))}
-                {surplusSynCount > 0 && (
-                  <span className="text-neutral-500 text-xs">+{surplusSynCount}</span>
-                )}
-              </div>
-            </div>
-          )}
+        <div className="flex items-center gap-1 text-xs text-neutral-500">
+          <FaProjectDiagram />
+          {totalSyncCount}
         </div>
       </div>
-    </Card>
+    )
+  }
+
+  const AppMetaRow = () => {
+    return (
+      <div
+        className={clsx(
+          variant === 'compact'
+            ? 'col-span-5 hidden lg:grid grid-cols-5 justify-stretch gap-4'
+            : 'flex items-center justify-between gap-2 w-full'
+        )}
+      >
+        <AppMetaCategory
+          itemType="Member"
+          count={members.length}
+          icon={<FaUsers />}
+          link={
+            variant === 'normal'
+              ? undefined
+              : `/${organisation?.name}/apps/${app.id}/access/members`
+          }
+        >
+          {members.slice(0, 5).map((member, index) => (
+            <div
+              key={member!.id}
+              className={clsx(
+                'rounded-full  transition ease',
+                variant === 'compact' && 'group-hover:saturate-50',
+                index !== 0 && '-ml-3',
+                index === 1 && 'z-[1]',
+                index === 2 && 'z-[2]',
+                index === 3 && 'z-[3]',
+                index === 4 && 'z-[4]'
+              )}
+            >
+              <Avatar showTitle={false} member={member!} size="md" />
+            </div>
+          ))}
+          {surplusMemberCount > 0 && (
+            <span className="text-neutral-500 text-xs">+{surplusMemberCount}</span>
+          )}
+        </AppMetaCategory>
+
+        <AppMetaCategory
+          itemType="Service Account"
+          count={serviceAccounts.length}
+          icon={<FaRobot />}
+          link={
+            variant === 'normal'
+              ? undefined
+              : `/${organisation?.name}/apps/${app.id}/access/service-accounts`
+          }
+        >
+          {serviceAccounts.slice(0, 5).map((account, index) => (
+            <div
+              key={account!.id}
+              className={clsx(
+                'bg-zinc-100 dark:bg-zinc-800  transition ease  ring-1 ring-inset ring-zinc-500/20 rounded-full size-8 flex items-center justify-center shrink-0 text-zinc-800 dark:text-zinc-200 text-2xs font-semibold',
+                variant === 'compact' && 'group-hover:bg-zinc-50 group-hover:dark:bg-zinc-700',
+                index !== 0 && '-ml-3',
+                index === 1 && 'z-[1]',
+                index === 2 && 'z-[2]',
+                index === 3 && 'z-[3]',
+                index === 4 && 'z-[4]'
+              )}
+            >
+              {account?.name.slice(0, 1)}
+            </div>
+          ))}
+          {surplusServiceAccountsCount > 0 && (
+            <span className="font-semibold text-zinc-900 dark:text-zinc-100 text-xs">
+              +{surplusServiceAccountsCount}
+            </span>
+          )}
+        </AppMetaCategory>
+
+        <AppMetaCategory
+          itemType="Environment"
+          count={environments.length}
+          icon={<BsListColumnsReverse />}
+          link={variant === 'normal' ? undefined : `/${organisation?.name}/apps/${app.id}/`}
+        >
+          {environments.slice(0, 5).map((env, index) => (
+            <div
+              key={env!.id}
+              className={clsx(
+                'bg-zinc-100 dark:bg-zinc-800  transition ease  ring-1 ring-inset ring-zinc-500/20 rounded-full size-8 flex items-center justify-center shrink-0 text-zinc-800 dark:text-zinc-200 text-2xs font-semibold',
+                variant === 'compact' && 'group-hover:bg-zinc-50 group-hover:dark:bg-zinc-700',
+                index !== 0 && '-ml-3',
+                index === 1 && 'z-[1]',
+                index === 2 && 'z-[2]',
+                index === 3 && 'z-[3]',
+                index === 4 && 'z-[4]'
+              )}
+            >
+              {env!.name.slice(0, 1)}
+            </div>
+          ))}
+          {surplusEnvCount > 0 && (
+            <span className="font-semibold text-zinc-900 dark:text-zinc-100 text-xs">
+              +{surplusEnvCount}
+            </span>
+          )}
+        </AppMetaCategory>
+
+        <AppMetaCategory
+          itemType="Integration"
+          count={totalSyncCount}
+          icon={<FaProjectDiagram />}
+          link={variant === 'normal' ? undefined : `/${organisation?.name}/apps/${app.id}/syncing`}
+        >
+          {providers.slice(0, 5).map((providerId) => (
+            <div
+              key={providerId}
+              className={clsx(
+                'text-2xl transition ease',
+                variant === 'compact' && 'group-hover:saturate-50'
+              )}
+            >
+              <ProviderIcon providerId={providerId} />
+            </div>
+          ))}
+          {surplusSynCount > 0 && (
+            <span className="font-semibold text-zinc-900 dark:text-zinc-100 text-xs">
+              +{surplusSynCount}
+            </span>
+          )}
+        </AppMetaCategory>
+
+        {variant === 'compact' && (
+          <div className="text-xs text-neutral-500 hidden lg:block">
+            {app.updatedAt && relativeTimeFromDates(new Date(app.updatedAt))}
+          </div>
+        )}
+      </div>
+    )
+  }
+
+  return (
+    <div
+      className={clsx(
+        'flex w-full',
+        variant === 'normal'
+          ? 'flex-col gap-8 justify-between rounded-xl'
+          : 'gap-6 lg:gap-10 grid grid-cols-2 lg:grid-cols-7 justify-stretch items-center py-1'
+      )}
+    >
+      <div
+        className={clsx(
+          'flex justify-between',
+          variant === 'normal' ? 'items-start' : 'items-center col-span-2 w-full lg:max-w-[24rem]'
+        )}
+      >
+        <div className="space-y-0.5 w-full">
+          <div
+            className={clsx(
+              'font-semibold flex items-center justify-between gap-2 text-zinc-700 dark:text-zinc-100 group-hover:text-zinc-900 dark:group-hover:text-zinc-100 transition ease',
+              variant === 'normal' ? 'text-2xl' : 'text-lg pl-3'
+            )}
+          >
+            {variant === 'normal' ? (
+              name
+            ) : (
+              <Link
+                href={`/${organisation?.name}/apps/${app.id}`}
+                className="flex items-center gap-4 justify-between w-full"
+              >
+                {name}
+                <div className="xl:hidden">
+                  <EncryptionModeIndicator app={app} />
+                </div>
+              </Link>
+            )}{' '}
+          </div>
+          {variant === 'normal' ? (
+            <div className="text-2xs font-mono text-neutral-500 w-full break-all text-left">
+              {id}
+            </div>
+          ) : (
+            <div className="flex items-center justify-between">
+              <div onClick={(e) => e.stopPropagation()}>
+                <CopyButton value={id} buttonVariant="ghost">
+                  <span className="text-2xs font-mono md:whitespace-nowrap text-neutral-500">
+                    {id}
+                  </span>
+                </CopyButton>
+              </div>
+              <div className="lg:hidden">
+                <CondensedAppMetaCounts />
+              </div>
+            </div>
+          )}
+        </div>
+        <div className="hidden xl:block">
+          <EncryptionModeIndicator app={app} />
+        </div>
+      </div>
+
+      {variant === 'normal' ? (
+        <div className="flex items-center justify-between p-2 gap-6">
+          <AppMetaRow />
+        </div>
+      ) : (
+        <AppMetaRow />
+      )}
+    </div>
   )
+}
+
+export const AppCard = ({ app, variant, tabToLink }: AppCardProps) => {
+  const { activeOrganisation: organisation } = useContext(organisationContext)
+  const router = useRouter()
+
+  const link = `/${organisation?.name}/apps/${app.id}/${tabToLink || ''}`
+
+  const handleRowClick = (e?: React.MouseEvent) => {
+    e?.stopPropagation()
+
+    // If ctrl/cmd key was pressed, open in new tab
+    if (e && (e.ctrlKey || e.metaKey)) {
+      window.open(link, '_blank')
+    } else {
+      router.push(link)
+    }
+  }
+
+  if (variant === 'normal')
+    return (
+      <Link href={link}>
+        <Card>
+          <AppCardContent app={app} variant={variant} />
+        </Card>
+      </Link>
+    )
+  else
+    return (
+      <div
+        className="hover:bg-neutral-500/10 transition ease cursor-pointer"
+        onClick={handleRowClick}
+      >
+        <AppCardContent app={app} variant={variant} />
+      </div>
+    )
 }
