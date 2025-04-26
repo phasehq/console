95a4ec0714afd956da08c3bd9f6abfedd7c3bfd9
Rohan <rohan.chaturvedi@protonmail.com>
2025-04-26 22:23:40 +0530

    feat: add empty state for global policies

--- frontend/app/[team]/access/network/page.tsx
+++ frontend/app/[team]/access/network/page.tsx
@@ -7,12 +7,13 @@ import { GetNetworkPolicies } from '@/graphql/queries/access/getNetworkPolicies.
 import { userHasPermission } from '@/utils/access/permissions'
 import { useQuery } from '@apollo/client'
 import { useContext } from 'react'
-import { FaBan, FaCheckCircle } from 'react-icons/fa'
+import { FaBan, FaCheckCircle, FaNetworkWired } from 'react-icons/fa'
 import { CreateNetworkAccessPolicyDialog } from './_components/CreateNetworkPolicyDialog'
 import { IPChip } from './_components/IPChip'
 import { UpdateNetworkAccessPolicyDialog } from './_components/UpdateNetworkPolicyDialog'
 import { DeleteNetworkAccessPolicyDialog } from './_components/DeleteNetworkPolicyDialog'
 import { ManageOrgGlobalPolicies } from './_components/ManageOrgGlobalPolicies'
+import { UpdateAccountNetworkPolicies } from '@/components/access/UpdateAccountNetworkPolicies'
 
 export default function NetworkPolicies({ params }: { params: { team: string } }) {
   const { activeOrganisation: organisation } = useContext(organisationContext)
@@ -111,7 +112,9 @@ export default function NetworkPolicies({ params }: { params: { team: string } }
         </div>
       </div>
 
-      <div className="w-full space-y-2 text-zinc-900 dark:text-zinc-100">
+      <hr className="border-neutral-500/40" />
+
+      <div className="w-full space-y-2 text-zinc-900 dark:text-zinc-100 pb-8">
         <div className="space-y-1">
           <h2 className="text-xl font-semibold">Global Policies</h2>
           <p className="text-neutral-500">
@@ -120,41 +123,64 @@ export default function NetworkPolicies({ params }: { params: { team: string } }
           </p>
         </div>
         <div className="space-y-4">
-          {userCanCreateNetworkPolicies && (
+          {userCanCreateNetworkPolicies && globalPolicies.length > 0 && (
             <div className="flex justify-end">
               <ManageOrgGlobalPolicies />
             </div>
           )}
 
           {userCanReadNetworkPolicies ? (
-            <table className="table-auto min-w-full divide-y divide-zinc-500/40 ">
-              <thead>
-                <tr>
-                  <th className="py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Name
-                  </th>
+            globalPolicies.length > 0 ? (
+              <table className="table-auto min-w-full divide-y divide-zinc-500/40 ">
+                <thead>
+                  <tr>
+                    <th className="py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Name
+                    </th>
 
-                  <th className="py-3 px-6 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Allowlist
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="divide-y divide-zinc-500/20">
-                {globalPolicies.map((policy: NetworkAccessPolicyType) => (
-                  <tr key={policy.id} className="group">
-                    <td className="text-zinc-900 dark:text-zinc-100 font-medium">{policy.name}</td>
-
-                    <td className="px-6 py-4">
-                      <div className="flex gap-2 flex-wrap">
-                        {policy.allowedIps.split(',').map((ip) => (
-                          <IPChip key={ip} ip={ip}></IPChip>
-                        ))}
-                      </div>
-                    </td>
+                    <th className="py-3 px-6 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Allowlist
+                    </th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody className="divide-y divide-zinc-500/20">
+                  {globalPolicies.map((policy: NetworkAccessPolicyType) => (
+                    <tr key={policy.id} className="group">
+                      <td className="text-zinc-900 dark:text-zinc-100 font-medium">
+                        {policy.name}
+                      </td>
+
+                      <td className="px-6 py-4">
+                        <div className="flex gap-2 flex-wrap">
+                          {policy.allowedIps.split(',').map((ip) => (
+                            <IPChip key={ip} ip={ip}></IPChip>
+                          ))}
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            ) : (
+              <EmptyState
+                title="No Global Polices"
+                subtitle={
+                  <>
+                    There are no global policies set for your organisation.
+                    <br /> Access is allowed from any IP address{' '}
+                    <span className="font-semibold font-mono">(0.0.0.0/0, ::/0)</span> for any
+                    accounts that do not have an explicit network access policy
+                  </>
+                }
+                graphic={
+                  <div className="text-neutral-300 dark:text-neutral-700 text-5xl text-center">
+                    <FaNetworkWired />
+                  </div>
+                }
+              >
+                <ManageOrgGlobalPolicies />
+              </EmptyState>
+            )
           ) : (
             <EmptyState
               title="Access restricted"
