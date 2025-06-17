62d4e6de73ee0c80bcd66a7992bb68742f96e133
rohan-chaturvedi <rohan.chaturvedi@protonmail.com>
2025-06-17 18:44:18 +0530

    fix: middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -9,6 +9,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico|favicon.svg|meta.png|login|lockbox|api/health).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|favicon.svg|assets|login|lockbox|api/health).*)',
   ],
 }
