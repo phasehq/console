f9a4e1fb9ae12ef5fc9d4005369b2768592b4afd
Rohan <rohan.chaturvedi@protonmail.com>
2025-07-16 12:01:13 +0530

    fix: google-oidc login handler

--- backend/ee/authentication/sso/oidc/util/google/views.py
+++ backend/ee/authentication/sso/oidc/util/google/views.py
@@ -3,8 +3,15 @@ from allauth.socialaccount.providers.oauth2.views import (
     OAuth2CallbackView,
 )
 from allauth.socialaccount import providers
+from allauth.socialaccount.providers.oauth2.views import OAuth2Error
 from api.authentication.adapters.generic.provider import GenericOpenIDConnectProvider
 from api.authentication.adapters.generic.views import GenericOpenIDConnectAdapter
+from django.conf import settings
+from django.utils import timezone
+import logging
+from api.models import ActivatedPhaseLicense
+
+logger = logging.getLogger(__name__)
 
 
 class GoogleOpenIDConnectProvider(GenericOpenIDConnectProvider):
@@ -22,6 +29,41 @@ class GoogleOpenIDConnectAdapter(GenericOpenIDConnectAdapter):
         "jwks_url": "https://www.googleapis.com/oauth2/v3/certs",
         "issuer": "https://accounts.google.com",
     }
+
+    def complete_login(self, request, app, token, **kwargs):
+        if settings.APP_HOST == "cloud":
+            error = "OIDC is not available in cloud mode"
+            logger.error(f"OIDC login failed: {str(error)}")
+            raise OAuth2Error(str(error))
+
+        # Check for a valid license
+        activated_license_exists = ActivatedPhaseLicense.objects.filter(
+            expires_at__gte=timezone.now()
+        ).exists()
+
+        if not activated_license_exists and not settings.PHASE_LICENSE:
+            error = "You need a license to login via OIDC."
+            logger.error(f"OIDC login failed: {str(error)}")
+            raise OAuth2Error(str(error))
+
+        try:
+            id_token = getattr(token, "id_token", None)
+            if not id_token and isinstance(kwargs.get("response"), dict):
+                id_token = kwargs["response"].get("id_token")
+
+            extra_data = self._get_user_data(token, id_token, app)
+            logger.debug(
+                f"User authentication data received for email: {extra_data.get('email')}"
+            )
+
+            # Create social login object without creating user
+            login = self.get_provider().sociallogin_from_response(request, extra_data)
+
+            return login
+
+        except Exception as e:
+            logger.error(f"OIDC login failed: {str(e)}")
+            raise OAuth2Error(str(e))
 
 
 oauth2_login = OAuth2LoginView.adapter_view(GoogleOpenIDConnectAdapter)
