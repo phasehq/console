7f198442fdc06677f0a88f19b8e9649df0b19055
rohan <rohan.chaturvedi@protonmail.com>
2025-09-02 16:38:04 +0530

    feat: enhance DynamicSecret component layout and add link to view secret
    Signed-off-by: rohan <rohan.chaturvedi@protonmail.com>

--- frontend/app/[team]/integrations/dynamic-secrets/_components/DynamicSecret.tsx
+++ frontend/app/[team]/integrations/dynamic-secrets/_components/DynamicSecret.tsx
@@ -1,37 +1,42 @@
 import { DynamicSecretType } from '@/apollo/graphql'
-import { CreateLeaseDialog } from '@/components/environments/secrets/dynamic/CreateLeaseDialog'
 import { DeleteDynamicSecretDialog } from '@/components/environments/secrets/dynamic/DeleteDynamicSecretDialog'
 import { ManageDynamicSecretDialog } from '@/components/environments/secrets/dynamic/ManageDynamicSecretDialog'
-import { FaBolt } from 'react-icons/fa6'
-import { UpdateDynamicSecretDialog } from './UpdateDynamicSecretDialog'
 import { ProviderIcon } from '@/components/syncing/ProviderIcon'
+import { Button } from '@/components/common/Button'
+import { organisationContext } from '@/contexts/organisationContext'
+import Link from 'next/link'
+import { useContext } from 'react'
+import { FaExternalLinkAlt } from 'react-icons/fa'
 
 export const DynamicSecret = ({ secret }: { secret: DynamicSecretType }) => {
+  const { activeOrganisation: organisation } = useContext(organisationContext)
+
   return (
-    <div className="p-2 ring-1 ring-inset ring-neutral-400/20 flex flex-col gap-4 w-full rounded-lg group justify-between">
-      <div className="">
+    <div className="p-2 ring-1 ring-inset ring-neutral-400/20 flex flex-col gap-4 w-full rounded-lg group justify-between bg-zinc-100 dark:bg-zinc-800">
+      <div>
         <div className="text-base text-zinc-900 dark:text-zinc-100 flex items-center gap-1 px-1">
           <ProviderIcon providerId={secret.provider} /> {secret.name}
         </div>
-        <div className="flex items-center gap-2">
-          {secret.keyMap?.map((key) => (
-            <span
-              key={key!.keyName}
-              className="font-mono text-neutral-500 text-xs bg-neutral-400/10 px-1 rounded-full"
-            >
-              {key!.keyName}
-            </span>
-          ))}
-        </div>
+        <div className="text-sm text-neutral-500 px-1">{secret.description}</div>
       </div>
+
       <div className="relative group flex items-center justify-between w-full">
         <div className="flex h-full items-center justify-start gap-4 opacity-0 group-hover:opacity-100 transition ease">
-          <CreateLeaseDialog secret={secret} />
+          {/* <CreateLeaseDialog secret={secret} /> */}
           <ManageDynamicSecretDialog secret={secret} />
-          <UpdateDynamicSecretDialog secret={secret} staticSecrets={[]} dynamicSecrets={[]} />
         </div>
         <div className="flex h-full items-center justify-end gap-4 opacity-0 group-hover:opacity-100 transition ease">
-          <DeleteDynamicSecretDialog secret={secret} />
+          {/* <DeleteDynamicSecretDialog secret={secret} /> */}
+          <Link
+            href={`/${organisation?.name}/apps/${secret.environment.app.id}/environments/${secret.environment.id}${secret.path}?secret=${secret.id}`}
+            title="View this secret"
+          >
+            <Button variant="ghost">
+              <span className="py-1">
+                <FaExternalLinkAlt />
+              </span>
+            </Button>
+          </Link>
         </div>
       </div>
     </div>
