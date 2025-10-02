dff980b8d7c2590ff562f982370581b2d6d1790a
rohan <rohan.chaturvedi@protonmail.com>
2025-10-02 18:16:31 +0530

    chore: combine ttl utils into single file
    Signed-off-by: rohan <rohan.chaturvedi@protonmail.com>

--- frontend/utils/ttl.ts
+++ frontend/utils/ttl.ts
@@ -83,3 +83,32 @@ export function isValidTTL(ttlString: string): boolean {
 export function getTTLExamples(): string[] {
   return ['60s', '10m', '2h', '7d']
 }
+
+export const MINIMUM_LEASE_TTL = 60 // 1 minute
+
+export const leaseTtlButtons = [
+  {
+    label: '15min',
+    seconds: '900',
+  },
+  {
+    label: '1h',
+    seconds: '3600',
+  },
+  {
+    label: '12h',
+    seconds: '43200',
+  },
+  {
+    label: '24h',
+    seconds: '86400',
+  },
+  {
+    label: '30d',
+    seconds: '2592000',
+  },
+  {
+    label: '90d',
+    seconds: '7776000',
+  },
+]
