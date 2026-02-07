179792723c728b9cdb5d1cea26d12b7f4196df3e
Nimish <nimish@phase.dev>
2026-02-07 21:41:45 +0530

    chore: eliminate KMS logs and app activity chart logic from backend schema

--- backend/backend/schema.py
+++ backend/backend/schema.py
@@ -171,7 +171,6 @@ from .graphene.mutations.app import (
     DeleteAppMutation,
     MemberType,
     RemoveAppMemberMutation,
-    RotateAppKeysMutation,
     UpdateAppNameMutation,
 )
 from .graphene.mutations.organisation import (
@@ -186,12 +185,10 @@ from .graphene.mutations.organisation import (
 from .graphene.types import (
     ActivatedPhaseLicenseType,
     AppType,
-    ChartDataPointType,
     EnvironmentKeyType,
     EnvironmentSyncType,
     EnvironmentTokenType,
     EnvironmentType,
-    KMSLogsResponseType,
     NetworkAccessPolicyType,
     OrganisationMemberInviteType,
     OrganisationMemberType,
@@ -211,7 +208,6 @@ from .graphene.types import (
     ServiceAccountType,
     ServiceTokenType,
     ServiceType,
-    TimeRange,
     UserTokenType,
     AWSValidationResultType,
     IdentityType,
@@ -235,10 +231,7 @@ from api.models import (
     ServiceToken,
     UserToken,
 )
-from logs.queries import get_app_log_count, get_app_log_count_range, get_app_logs
 from datetime import datetime, timedelta
-from django.conf import settings
-from logs.models import KMSDBLog
 from django.utils import timezone
 from itertools import chain
 import time
@@ -247,8 +240,6 @@ import heapq
 from django.db.models import prefetch_related_objects
 
 logger = logging.getLogger(__name__)
-
-CLOUD_HOSTED = settings.APP_HOST == "cloud"
 
 
 class Query(graphene.ObjectType):
@@ -292,13 +283,6 @@ class Query(graphene.ObjectType):
         AppType, organisation_id=graphene.ID(), app_id=graphene.ID(required=False)
     )
 
-    kms_logs = graphene.Field(
-        KMSLogsResponseType,
-        app_id=graphene.ID(),
-        start=graphene.BigInt(),
-        end=graphene.BigInt(),
-    )
-
     secret_logs = graphene.Field(
         SecretLogsResponseType,
         app_id=graphene.ID(),
@@ -308,12 +292,6 @@ class Query(graphene.ObjectType):
         member_id=graphene.ID(),
         member_type=MemberType(),
         environment_id=graphene.ID(),
-    )
-
-    app_activity_chart = graphene.List(
-        ChartDataPointType,
-        app_id=graphene.ID(),
-        period=graphene.Argument(graphene.Enum.from_enum(TimeRange)),
     )
 
     app_environments = graphene.List(
@@ -794,44 +772,6 @@ class Query(graphene.ObjectType):
     resolve_service_accounts = resolve_service_accounts
     resolve_service_account_handlers = resolve_service_account_handlers
 
-    def resolve_kms_logs(root, info, app_id, start=0, end=0):
-        if not user_can_access_app(info.context.user.userId, app_id):
-            raise GraphQLError("You don't have access to this app")
-
-        app = App.objects.get(id=app_id)
-
-        if end == 0:
-            end = datetime.now().timestamp() * 1000
-
-        if CLOUD_HOSTED:
-            try:
-                kms_logs = get_app_logs(
-                    f"phApp:v{app.app_version}:{app.identity_key}", start, end, 25
-                )
-                count = get_app_log_count(
-                    f"phApp:v{app.app_version}:{app.identity_key}"
-                )
-            except:
-                print("Error fetching KMS logs")
-                kms_logs = []
-                count = 0
-
-        else:
-            kms_logs = list(
-                KMSDBLog.objects.filter(
-                    app_id=f"phApp:v{app.app_version}:{app.identity_key}",
-                    timestamp__lte=end,
-                    timestamp__gte=start,
-                )
-                .order_by("-timestamp")[:25]
-                .values()
-            )
-            count = KMSDBLog.objects.filter(
-                app_id=f"phApp:v{app.app_version}:{app.identity_key}"
-            ).count()
-
-        return SecretLogsResponseType(logs=kms_logs, count=count)
-
     def resolve_secret_logs(
         root,
         info,
@@ -945,92 +885,6 @@ class Query(graphene.ObjectType):
 
         return SecretLogsResponseType(logs=logs_qs, count=count)
 
-    def resolve_app_activity_chart(root, info, app_id, period=TimeRange.DAY):
-        """
-        Converts app log activity for the chosen time period into time series data that can be used to draw a chart
-        Args:
-            app_id (string): app uuid
-            period (TimeRange, optional): The desired time period. Defaults to 'day'.
-        Raises:
-            GraphQLError: If the requesting user does not have access to this app
-        Returns:
-            List[ChartDataPointType]: Time series decrypt count data
-        """
-
-        app = App.objects.get(id=app_id)
-        if not user_can_access_app(info.context.user.userId, app_id):
-            raise GraphQLError("You don't have access to this app")
-
-        end_date = datetime.now()  # current time
-
-        # default values for period='day'
-        # 24 hours before current time
-        start_date = end_date - timedelta(hours=24)
-        time_iteration = timedelta(hours=1)
-
-        match period:
-            case TimeRange.HOUR:
-                # 7 days before current time
-                start_date = end_date - timedelta(hours=1)
-                time_iteration = timedelta(minutes=5)
-            case TimeRange.WEEK:
-                # 7 days before current time
-                start_date = end_date - timedelta(days=7)
-                time_iteration = timedelta(days=1)
-            case TimeRange.MONTH:
-                # 30 days before current time
-                start_date = end_date - timedelta(days=30)
-                time_iteration = timedelta(days=1)
-            case TimeRange.YEAR:
-                # 365 days before current time
-                start_date = end_date - timedelta(days=365)
-                time_iteration = timedelta(days=5)
-            case TimeRange.ALL_TIME:
-                # 365 days before current time
-                start_date = end_date - timedelta(days=365)
-                time_iteration = timedelta(days=7)
-
-        time_series_logs = []
-
-        # initialize the iterators
-        current_date = start_date
-        index = 0
-
-        # loop through each iteration in the period and calculate the number of decrypts per time_iteration
-        while current_date <= end_date:
-            # Get the start and end of the current measurement period as datetime objects
-            start_of_measurement_period = current_date.replace(second=0, microsecond=0)
-            if (current_date + time_iteration) > end_date:
-                end_of_measurement_period = end_date
-            else:
-                end_of_measurement_period = start_of_measurement_period + time_iteration
-
-            # Convert the start and end of the measurement period to unix timestamps
-            start_unix = int(start_of_measurement_period.timestamp() * 1000)
-            end_unix = int(end_of_measurement_period.timestamp() * 1000)
-
-            # Get the count of decrypts in the measurement period
-            if CLOUD_HOSTED:
-                decrypts = get_app_log_count_range(
-                    f"phApp:v{app.app_version}:{app.identity_key}", start_unix, end_unix
-                )
-            else:
-                decrypts = KMSDBLog.objects.filter(
-                    app_id=f"phApp:v{app.app_version}:{app.identity_key}",
-                    timestamp__lte=end_unix,
-                    timestamp__gte=start_unix,
-                ).count()
-
-            time_series_logs.append(
-                ChartDataPointType(index=str(index), date=end_unix, data=decrypts)
-            )
-
-            # Increment current_date by one time iteration
-            current_date += time_iteration
-            index += 1
-
-        return time_series_logs
-
     resolve_stripe_checkout_details = resolve_stripe_checkout_details
     resolve_stripe_subscription_details = resolve_stripe_subscription_details
     resolve_stripe_customer_portal_url = resolve_stripe_customer_portal_url
@@ -1048,7 +902,6 @@ class Mutation(graphene.ObjectType):
     delete_invitation = DeleteInviteMutation.Field()
 
     create_app = CreateAppMutation.Field()
-    rotate_app_keys = RotateAppKeysMutation.Field()
     delete_app = DeleteAppMutation.Field()
     update_app_name = UpdateAppNameMutation.Field()
     add_app_member = AddAppMemberMutation.Field()
