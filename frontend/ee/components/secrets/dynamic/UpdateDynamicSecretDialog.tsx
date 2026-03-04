1f763f45ce9a71da5c1d7d0899928c16460ed75b
rohan <rohan.chaturvedi@protonmail.com>
2026-03-05 01:06:25 +0530

    fix: update dynamic secret dialog title
    Signed-off-by: rohan <rohan.chaturvedi@protonmail.com>

--- frontend/ee/components/secrets/dynamic/UpdateDynamicSecretDialog.tsx
+++ frontend/ee/components/secrets/dynamic/UpdateDynamicSecretDialog.tsx
@@ -171,7 +171,7 @@ export const UpdateDynamicSecretDialog = forwardRef<
       ref={dialogRef}
       title="Configure Dynamic Secret"
       dialogTitle={
-        <div className="flex flex-col gap-1">
+        <div className="flex flex-col gap-1 text-zinc-900 dark:text-zinc-100">
           Configure {secret.name}
           <div className="flex items-center gap-1 text-xs text-neutral-500">
             Dynamic Secret ID:
