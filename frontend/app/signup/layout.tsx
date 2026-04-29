02ba8279aef8d70f21e8d975f485cc019f767bcf
rohan <rohan.chaturvedi@protonmail.com>
2026-04-29 17:01:27 +0530

    fix: signup navbar
    Signed-off-by: rohan <rohan.chaturvedi@protonmail.com>

--- frontend/app/signup/layout.tsx
+++ frontend/app/signup/layout.tsx
@@ -1,11 +1,28 @@
 import '@/app/globals.css'
-import OnboardingNavbar from '@/components/layout/OnboardingNavbar'
-import UserMenu from '@/components/UserMenu'
+import { InstanceInfo } from '@/components/InstanceInfo'
+import { ModeToggle } from '@/components/common/ModeToggle'
+import { StatusIndicator } from '@/components/common/StatusIndicator'
+import { isCloudHosted } from '@/utils/appConfig'
+import { FaSun, FaMoon } from 'react-icons/fa6'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className={`w-full min-h-screen`}>
-      <OnboardingNavbar />
+    <div className="relative w-full min-h-screen">
+      <div className="absolute top-4 px-4 md:px-8 md:top-8 w-full flex justify-between gap-6 z-10">
+        <div className="flex items-center gap-2">
+          <InstanceInfo />
+        </div>
+        <div className="flex items-center gap-6">
+          {isCloudHosted() && <StatusIndicator />}
+          <div className="flex items-center justify-between px-2 text-neutral-500">
+            <div className="flex items-center gap-2">
+              <FaSun />
+              <ModeToggle />
+              <FaMoon />
+            </div>
+          </div>
+        </div>
+      </div>
       {children}
     </div>
   )
