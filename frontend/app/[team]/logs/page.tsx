63ce58cd44991e2f6811fcd0e916473f1bdbb7a0
Nimish <nimish@phase.dev>
2026-05-20 15:34:14 +0530

    feat: rename the page title to audit logs

--- frontend/app/[team]/logs/page.tsx
+++ frontend/app/[team]/logs/page.tsx
@@ -17,7 +17,7 @@ export default function OrgLogs({ params }: { params: { team: string } }) {
 
   return (
     <div className="h-screen overflow-y-auto w-full text-black dark:text-white flex flex-col px-3 sm:px-4 lg:px-6">
-      <h1 className="text-lg sm:text-xl font-bold pt-3 sm:pt-4 lg:pt-6 pb-3 sm:pb-4 lg:pb-6">Logs</h1>
+      <h1 className="text-lg sm:text-xl font-bold pt-3 sm:pt-4 lg:pt-6 pb-3 sm:pb-4 lg:pb-6">Audit Logs</h1>
       <AuditLogs />
     </div>
   )
