53a0c291a83eb5fa1117bb6a74a47d505748def2
Nimish <nimish@phase.dev>
2026-09-15 14:17:27 +0530

    feat: added psql provider icon

--- frontend/components/syncing/ProviderIcon.tsx
+++ frontend/components/syncing/ProviderIcon.tsx
@@ -7,6 +7,7 @@ import {
   SiGitlab,
   SiNomad,
   SiOpenai,
+  SiPostgresql,
   SiRailway,
   SiSupabase,
   SiVault,
@@ -61,5 +62,6 @@ export const ProviderIcon = (props: { providerId: string }) => {
       </span>
     )
 
+  if (id.includes('postgres')) return <SiPostgresql className="shrink-0 text-[#4169E1]" />
   else return <FaCube />
 }
